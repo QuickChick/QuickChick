b419befab88a348c6f7dc4d23670dbcb44e0681c
Catalin Hritcu <catalin.hritcu@gmail.com>
2014-06-13 17:36:41 +0200

    Small polishing

--- src/SetOfOutcomes.v
+++ src/SetOfOutcomes.v
@@ -1,9 +1,17 @@
 Require Import AbstractGen.
 Require Import List ssreflect ssrbool ssrnat seq.
 
-Definition SO A := A -> Prop. 
+Definition Pred (A : Type) := A -> Prop.
 
-Program Instance GenMonad : GenMonad SO :=
+(* Equivalence on sets of outcomes *) 
+Definition peq {A} (m1 m2 : Pred A) := 
+  forall A, m1 A <-> m2 A.
+
+(* the set of outcomes m1 is a subset of m2 *) 
+Definition pincl {A} (m1 m2 : Pred A) :=
+  forall A, m1 A -> m2 A. 
+
+Program Instance GenMonad : GenMonad Pred :=
   {
     bindGen A B ma f := 
       fun b => exists a, ma a /\ f a b;
@@ -18,16 +26,3 @@ Program Instance GenMonad : GenMonad SO :=
     sized A f := 
       fun a => exists n, f n a
   }.
-
-(* Equivalence on sets of outcomes *) 
-Definition SOeq {A} (m1 m2 : SO A) := 
-  forall A, m1 A <-> m2 A.
-
-(* the set of outcomes m1 is a subset of m2 *) 
-Definition SOincl {A} (m1 m2 : SO A) :=
-  forall A, m1 A -> m2 A. 
-
-
-  
-   
-   
