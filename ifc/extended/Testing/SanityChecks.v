04e337a4ed74126674052ec3a54426a9b7908c64
Catalin Hritcu <catalin.hritcu@gmail.com>
2014-08-13 20:32:52 +0200

    One more proved checker (a trivial one)

--- ifc/extended/Testing/SanityChecks.v
+++ ifc/extended/Testing/SanityChecks.v
@@ -6,9 +6,7 @@ Require Import SingleStateArb.
 Require Import Indist.
 Require Import Shrinking.
 Require Import Generation.
-
 Require Import Machine.
-Import Mem.
 
 Require Import List.
 Require Import Common.
@@ -18,29 +16,28 @@ Require Import ssreflect ssrbool eqtype.
 
 Local Open Scope string.
 
-(* Sanity check for stamp generation *)
-Definition prop_stamp_generation (st : State) : Property Gen.Gen :=
-  (* whenFail (show st) *) (property (well_formed st)).
-
-(*
-Definition propStampGeneration (st : State) :=
-  let stamps := generateStamps st in
-  whenFail (Property.trace ("Generated: " ++ nl ++
-                           (showStamps (allocated (st_mem st)) stamps)))
-           (wellFormed st stamps).
-*)
-
-Definition prop_generate_indist : Property Gen.Gen :=
-  forAllShrink show gen_variation_state (fun _ => []) (* shrinkVState *)
-               (fun v => let '(Var lab st1 st2) := v in
-                property (indist lab st1 st2) : Gen.Gen QProp).
-
 Section Checkers.
   Context {Gen : Type -> Type}
           {H : GenMonad Gen}.
 
-  Definition prop_exec_preserves_well_formed (t : table)
-  : Property Gen :=
+  (* Sanity check for stamp generation *)
+  Definition prop_stamp_generation (st : State) : Property Gen :=
+    (* whenFail (show st) *) (property (well_formed st)).
+
+  (*
+  Definition propStampGeneration (st : State) :=
+    let stamps := generateStamps st in
+    whenFail (Property.trace ("Generated: " ++ nl ++
+                             (showStamps (allocated (st_mem st)) stamps)))
+             (wellFormed st stamps).
+  *)
+
+  Definition prop_generate_indist : Property Gen :=
+    forAllShrink show gen_variation_state (fun _ => []) (* shrinkVState *)
+                 (fun v => let '(Var lab st1 st2) := v in
+                  property (indist lab st1 st2) : Gen QProp).
+
+  Definition prop_exec_preserves_well_formed (t : table) : Property Gen :=
     forAllShrink (* show *)(fun _ => ""%string) arbitrary (fun _ => []) (fun st =>
     (if well_formed st then
       match exec t st with
@@ -56,20 +53,36 @@ Section Checkers.
 
 End Checkers.
 
-Definition exec_preserves_well_formed t : Prop := forall st x st',
-  well_formed st ->
-  exec t st = Some (x, st') ->
-  well_formed st'.
+(* TODO: get a way to still run these tests!
+Definition xxx := forAll show arbitrary (prop_stamp_generation : State -> Gen.Gen QProp).
+QuickCheck xxx. Could not run test
+*)
 
 Require Import EndToEnd SetOfOutcomes.
 
-(* One direction of this (soundness) checked by prop_stamp_generation above;
+(* This is rather trivial, but it was mentioned below so I've proved it *)
+Lemma prop_stamp_generation_equiv :
+  semTestable (prop_stamp_generation : State -> Pred QProp) <->
+  (forall st, @genState Pred _ st -> well_formed st).
+Proof.
+  rewrite /prop_stamp_generation /semTestable /property /testFun.
+  setoid_rewrite semForAllShrink. split => H st gen.
+  - specialize (H st gen). by move /semPredQProp/semBool in H.
+  - rewrite semPredQProp. setoid_rewrite <- semBool. apply H. exact gen.
+Qed.
+
+(* One direction of this (soundness) is checked by prop_stamp_generation above;
    this is the high-level spec we need for genState *)
 Lemma genState_well_formed : forall st,
   @genState Pred _ st <-> well_formed st.
 Admitted.
 
-Lemma prop_exec_preserves_well_formed_equiv:
+Definition exec_preserves_well_formed t : Prop := forall st x st',
+  well_formed st ->
+  exec t st = Some (x, st') ->
+  well_formed st'.
+
+Lemma prop_exec_preserves_well_formed_equiv :
   forall (t : table),
     semProperty (@prop_exec_preserves_well_formed Pred _ t) <->
     exec_preserves_well_formed t.
