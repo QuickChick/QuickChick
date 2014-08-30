8cb298bc173d1c47694cdea3d7d810bf51a6bd51
Catalin Hritcu <catalin.hritcu@gmail.com>
2014-08-30 11:39:27 +0200

    Renamed to match paper

--- ifc/extended/NotionsOfNI.v
+++ ifc/extended/NotionsOfNI.v
@@ -6,32 +6,32 @@ Section Everything.
 
 Variable A : Type.
 Variable low : pred A.
-Variable initial : pred A.
-Variable halted : pred A.
+Variable start : pred A.
+Variable ended : pred A.
 Variable step : A -> option A.
-Variable equiv : rel A.
+Variable indist : rel A.
 
-Hypothesis equivT : transitive equiv.
+Hypothesis indistT : transitive indist.
 
-Hypothesis equivS : symmetric equiv.
+Hypothesis indistS : symmetric indist.
 
-Hypothesis halted_low : subpred halted low.
+Hypothesis ended_low : subpred ended low.
 
-Hypothesis halted_equiv :
+Hypothesis ended_indist :
   forall s1 s2,
-    equiv s1 s2 ->
-    halted s1 = halted s2.
+    indist s1 s2 ->
+    ended s1 = ended s2.
 
-Hypothesis equiv_low :
+Hypothesis indist_low :
   forall s1 s2,
-    equiv s1 s2 ->
+    indist s1 s2 ->
     low s1 = low s2.
 
 Definition high := [pred s | ~~ low s].
 
 Definition stuck := [pred s | ~~ step s].
 
-Hypothesis halted_stuck : subpred halted stuck.
+Hypothesis ended_stuck : subpred ended stuck.
 
 Fixpoint exec (n : nat) (s : A) : A :=
   match n, step s with
@@ -53,67 +53,67 @@ Proof.
   case: n => [|n] //=.
 Qed.
 
-Lemma trace_halted n s :
-  halted s -> trace n s = [:: s].
+Lemma trace_ended n s :
+  ended s -> trace n s = [:: s].
 Proof.
 move => H; case: n => [|n] //=.
-move/halted_stuck: H.
+move/ended_stuck: H.
 by rewrite /stuck /=; case: (step s).
 Qed.
 
-Lemma exec_halted n s :
-  halted s -> exec n s = s.
+Lemma exec_ended n s :
+  ended s -> exec n s = s.
 Proof.
   move => H.
-  by rewrite (exec_trace s) (trace_halted _ H).
+  by rewrite (exec_trace s) (trace_ended _ H).
 Qed.
 
 Definition eeni : Prop :=
   forall (s1 s2 : A) (n : nat),
-    initial s1 ->
-    initial s2 ->
-    equiv s1 s2 ->
-    halted (exec n s1) ->
-    halted (exec n s2) ->
-    equiv (exec n s1) (exec n s2).
+    start s1 ->
+    start s2 ->
+    indist s1 s2 ->
+    ended (exec n s1) ->
+    ended (exec n s2) ->
+    indist (exec n s1) (exec n s2).
 
-Inductive equivt : seq A -> seq A -> Prop :=
-| EquivtNil : forall t, equivt [::] t
-| EquivtConsLow : forall x1 x2 t1 t2,
-                    equiv x1 x2 -> equivt t1 t2 ->
+Inductive indistt : seq A -> seq A -> Prop :=
+| IndisttNil : forall t, indistt [::] t
+| IndisttConsLow : forall x1 x2 t1 t2,
+                    indist x1 x2 -> indistt t1 t2 ->
                     low x1 -> low x2 ->
-                    equivt (x1 :: t1) (x2 :: t2)
-| EquivtConsHigh : forall x1 t1 t2,
-                     equivt t1 t2 ->
+                    indistt (x1 :: t1) (x2 :: t2)
+| IndisttConsHigh : forall x1 t1 t2,
+                     indistt t1 t2 ->
                      high x1 ->
-                     equivt (x1 :: t1) t2
-| EquivtSym : forall t1 t2,
-                equivt t1 t2 ->
-                equivt t2 t1.
+                     indistt (x1 :: t1) t2
+| IndisttSym : forall t1 t2,
+                indistt t1 t2 ->
+                indistt t2 t1.
 
-Definition equivtb (t1 t2 : seq A) : bool :=
-  all id (map (prod_curry equiv) (zip (filter low t1) (filter low t2))).
+Definition indisttb (t1 t2 : seq A) : bool :=
+  all id (map (prod_curry indist) (zip (filter low t1) (filter low t2))).
 
-Lemma equivtbtN t : equivtb t [::].
+Lemma indisttbtN t : indisttb t [::].
 Proof.
   case: t => [//=|x t] /=.
-  rewrite /equivtb /=.
+  rewrite /indisttb /=.
   case: (low x); first by [].
   by case: ([seq x <- t | low x]).
 Qed.
 
-Lemma equivtbNt t : equivtb [::] t.
+Lemma indisttbNt t : indisttb [::] t.
   case: t => [//=|x t] /=.
-  rewrite /equivtb /=.
+  rewrite /indisttb /=.
   case: (low x); first by [].
   by case: ([seq x <- t | low x]).
 Qed.
 
-Lemma equivt_cons x1 x2 t1 t2 :
-  equivt (x1 :: t1) (x2 :: t2) ->
-  [\/ [/\ equiv x1 x2, low x1, low x2 & equivt t1 t2],
-      [/\ equivt t1 (x2 :: t2) & high x1] |
-      [/\ equivt (x1 :: t1) t2 & high x2] ].
+Lemma indistt_cons x1 x2 t1 t2 :
+  indistt (x1 :: t1) (x2 :: t2) ->
+  [\/ [/\ indist x1 x2, low x1, low x2 & indistt t1 t2],
+      [/\ indistt t1 (x2 :: t2) & high x1] |
+      [/\ indistt (x1 :: t1) t2 & high x2] ].
 Proof.
   move: {1 3}(x1 :: t1) (erefl (x1 :: t1))
         {1 3}(x2 :: t2) (erefl (x2 :: t2))
@@ -127,68 +127,68 @@ Proof.
     split; last by [].
     congruence.
   - move => x1 t1' H1 x2 t2' H2.
-    move: IH => /(_ _ _ H2 _ _ H1) [[IH1 IH2 IH3 /(@EquivtSym _) IH4]|[IH1 IH2]|[IH1 IH2]].
+    move: IH => /(_ _ _ H2 _ _ H1) [[IH1 IH2 IH3 /(@IndisttSym _) IH4]|[IH1 IH2]|[IH1 IH2]].
     + apply Or31.
-      rewrite equivS in IH1.
+      rewrite indistS in IH1.
       by constructor.
     + apply Or33. split; last by [].
-      apply EquivtSym. congruence.
+      apply IndisttSym. congruence.
     + apply Or32. split; last by [].
-      apply EquivtSym. congruence.
+      apply IndisttSym. congruence.
 Qed.
 
-Lemma equivt_consH x1 t1 t2 :
+Lemma indistt_consH x1 t1 t2 :
   high x1 ->
-  equivt (x1 :: t1) t2 ->
-  equivt t1 t2.
+  indistt (x1 :: t1) t2 ->
+  indistt t1 t2.
 Proof.
   move => H1.
   move: {2}(size t1 + size t2) (leqnn (size t1 + size t2)) => n Hn.
   elim: n t1 t2 Hn => [t1 t2|n IH t1].
   - by rewrite leqn0 addn_eq0; case: t1; case:t2 => //; constructor.
   - case=> [|x2 t2] Hn E.
-    { apply EquivtSym. by constructor. }
-    move: E => /(@equivt_cons _) [[_ E _ _]|[E _] //=|[E1 E2]].
+    { apply IndisttSym. by constructor. }
+    move: E => /(@indistt_cons _) [[_ E _ _]|[E _] //=|[E1 E2]].
     + by rewrite /high /= E in H1.
-    + apply EquivtSym. apply EquivtConsHigh; last by [].
-      apply EquivtSym. apply IH; last by [].
+    + apply IndisttSym. apply IndisttConsHigh; last by [].
+      apply IndisttSym. apply IH; last by [].
       by rewrite /= addnS ltnS in Hn.
 Qed.
 
-Lemma equivtb_single s1 s2 : equiv s1 s2 -> equivtb [:: s1] [:: s2].
+Lemma indisttb_single s1 s2 : indist s1 s2 -> indisttb [:: s1] [:: s2].
 Proof.
   move => H.
-  rewrite /equivtb /= (equiv_low H).
+  rewrite /indisttb /= (indist_low H).
   case: (low s2) => //=.
   by rewrite andbT.
 Qed.
 
-Lemma equivtb_cons2 s1 t1 s2 t2 : equiv s1 s2 -> equivtb (s1 :: t1) (s2 :: t2) = equivtb t1 t2.
+Lemma indisttb_cons2 s1 t1 s2 t2 : indist s1 s2 -> indisttb (s1 :: t1) (s2 :: t2) = indisttb t1 t2.
 Proof.
   move => H.
-  rewrite /equivtb /= (equiv_low H).
+  rewrite /indisttb /= (indist_low H).
   case: (low s2) => //=.
   by rewrite H.
 Qed.
 
-Lemma equivtP t1 t2 : reflect (equivt t1 t2) (equivtb t1 t2).
+Lemma indisttP t1 t2 : reflect (indistt t1 t2) (indisttb t1 t2).
 Proof.
   move: {2}(size t1 + size t2) (leqnn (size t1 + size t2)) => n Hn.
   elim: n t1 t2 Hn => [|n IH] t1 t2.
   - by rewrite leqn0 addn_eq0; case: t1; case: t2 => //; do 2! constructor.
   - case: t1 => [|x1 t1].
-    { rewrite equivtbNt.
+    { rewrite indisttbNt.
       by do 2! constructor. }
     case: t2 => [|x2 t2] Hn.
-    { rewrite equivtbtN.
-      constructor. apply EquivtSym. by constructor. }
-    rewrite /equivtb /=.
+    { rewrite indisttbtN.
+      constructor. apply IndisttSym. by constructor. }
+    rewrite /indisttb /=.
     have [L1|L1] := (boolP (low x1)); have [L2|L2] := (boolP (low x2)) => /=.
     + rewrite /= addSn addnS ltnS ltn_neqAle in Hn.
       move: Hn => /andP [_ /(IH _) Hn] {IH}.
       apply/(iffP idP).
       * move/andP => [E1 /Hn E2]. by constructor.
-      * move => /(@equivt_cons _) H.
+      * move => /(@indistt_cons _) H.
         rewrite /high /= L1 L2 in H.
         case: H => //=.
         move => [-> _ _ H] /=.
@@ -197,63 +197,66 @@ Proof.
     + by move => [_ ?].
     + rewrite [size (x2 :: t2)]/= addnS ltnS in Hn.
       move: Hn => /(IH _) {IH} /= IH.
-      rewrite /equivtb /= L1 in IH.
+      rewrite /indisttb /= L1 in IH.
       apply/(iffP IH) => H.
-      * apply EquivtSym. apply EquivtConsHigh; try done.
-        by apply EquivtSym.
-      * apply EquivtSym.
-        apply (@equivt_consH x2); first by [].
-        by apply EquivtSym.
+      * apply IndisttSym. apply IndisttConsHigh; try done.
+        by apply IndisttSym.
+      * apply IndisttSym.
+        apply (@indistt_consH x2); first by [].
+        by apply IndisttSym.
     + rewrite [size (x1 :: t1)]/= addSn ltnS in Hn.
       move: Hn => /(IH _) {IH} Hn.
-      rewrite /equivtb /= L2 /= in Hn.
+      rewrite /indisttb /= L2 /= in Hn.
       apply (iffP Hn) => H.
-      * by apply EquivtConsHigh.
-      * by apply (@equivt_consH x1).
+      * by apply IndisttConsHigh.
+      * by apply (@indistt_consH x1).
     + rewrite /= addSn addnS ltnS ltn_neqAle in Hn.
       move: Hn => /andP [_ /(IH _) Hn] {IH}.
       apply (iffP Hn) => H.
-      * apply EquivtConsHigh; last by [].
-        apply EquivtSym.
-        apply EquivtConsHigh; last by [].
-        by apply EquivtSym.
-      * apply (@equivt_consH x1); first by [].
-        apply EquivtSym.
-        apply (@equivt_consH x2); first by [].
-        by apply EquivtSym.
+      * apply IndisttConsHigh; last by [].
+        apply IndisttSym.
+        apply IndisttConsHigh; last by [].
+        by apply IndisttSym.
+      * apply (@indistt_consH x1); first by [].
+        apply IndisttSym.
+        apply (@indistt_consH x2); first by [].
+        by apply IndisttSym.
 Qed.
 
+(* CH: It's nasty to expose a quantification over n in the property;
+       there are much better ways to do this (equivalent but less proof-oriented);
+       see how we define things in the paper *)
 Definition llni : Prop :=
   forall (s1 s2 : A) (n : nat),
-    initial s1 ->
-    initial s2 ->
-    equiv s1 s2 ->
-    equivtb (trace n s1) (trace n s2).
+    start s1 ->
+    start s2 ->
+    indist s1 s2 ->
+    indisttb (trace n s1) (trace n s2).
 
 Record ssni : Prop := {
 
   ssni_low_low : forall s1 s2 s1' s2',
                    low s1 ->
-                   equiv s1 s2 ->
+                   indist s1 s2 ->
                    step s1 = Some s1' ->
                    step s2 = Some s2' ->
-                   equiv s1' s2';
+                   indist s1' s2';
 
   ssni_high_high : forall s s',
                      high s -> high s' ->
                      step s = Some s' ->
-                     equiv s s';
+                     indist s s';
 
   ssni_high_low : forall s1 s2 s1' s2',
                     high s1 ->
-                    equiv s1 s2 ->
+                    indist s1 s2 ->
                     low s1' -> low s2' ->
                     step s1 = Some s1' ->
                     step s2 = Some s2' ->
-                    equiv s1' s2'
+                    indist s1' s2'
 
   (* The fourth condition is now redundant given the assumption about
-     equivalence and halted states, which seems to be needed for the
+     indistalence and ended states, which seems to be needed for the
      LLNI -> EENI proof. *)
 
 }.
@@ -261,7 +264,7 @@ Record ssni : Prop := {
 Lemma llni_eeni : llni -> eeni.
 Proof.
   move => LLNI s1 s2 n I1 I2 E12 H1 H2.
-  move: LLNI => /(_ s1 s2 n I1 I2 E12)/equivtP LLNI {I1 I2}.
+  move: LLNI => /(_ s1 s2 n I1 I2 E12)/indisttP LLNI {I1 I2}.
   move: {1 3}(trace n s1) {1 3}(trace n s2) (erefl (trace n s1)) (erefl (trace n s2)) LLNI
         => t1 t2 Ht1 Ht2 LLNI {E12}.
   elim: LLNI n {2 4 6}(n) s1 s2 H1 H2 Ht1 Ht2
@@ -273,28 +276,28 @@ Proof.
     + by move: Ht1 Ht2 Ex => [-> ?] [-> ?].
     + move: Ht1 Ex Hx1 => [-> _] Ex Hx1.
       case S2: (step s2) s2' Ht2 H1 Hx2 H2 Ex => [s2'|] s2'' /= [-> Ht2] {s2''} H1 Hx2 H2 Ex; last by [].
-      rewrite (halted_equiv Ex) in H1.
-      by have := halted_stuck H1; rewrite /stuck /= S2.
+      rewrite (ended_indist Ex) in H1.
+      by have := ended_stuck H1; rewrite /stuck /= S2.
     + move: Ht2 Ex Hx2 => [-> _] Ex Hx2.
       case S1: (step s1) s1' Ht1 H2 Hx1 H1 Ex => [s1'|] s1'' /= [-> Ht1] {s1''} H2 Hx1 H1 Ex; last by [].
-      rewrite -(halted_equiv Ex) in H2.
-      by have := halted_stuck H2; rewrite /stuck /= S1.
+      rewrite -(ended_indist Ex) in H2.
+      by have := ended_stuck H2; rewrite /stuck /= S1.
     + case S1: (step s1) s1' Ht1 Ex Hx1 H1 => [s1'|] s1'' [-> ?] {s1''} Ex Hx1 H1;
       case S2: (step s2) s2' Ht2 Ex Hx2 H2 => [s2'|] s2'' [-> ?] {s2''} Ex Hx2 H2 //=.
       * by apply IH.
-      * rewrite -(halted_equiv Ex) in H2.
-        by have := halted_stuck H2; rewrite /stuck /= S1.
-      * rewrite (halted_equiv Ex) in H1.
-        by have := halted_stuck H1; rewrite /stuck /= S2.
+      * rewrite -(ended_indist Ex) in H2.
+        by have := ended_stuck H2; rewrite /stuck /= S1.
+      * rewrite (ended_indist Ex) in H1.
+        by have := ended_stuck H1; rewrite /stuck /= S2.
   - case: n1 H1 Ht1 => [|n1] //= H1 Ht1.
     { move: Ht1 Hx1 => [-> _] Hx1.
-      by rewrite /halted /= (halted_low H1) in Hx1. }
+      by rewrite /ended /= (ended_low H1) in Hx1. }
     case S1: (step s1) s1' Ht1 Hx1 H1 => [s1'|] s1'' [-> Ht1] {s1''} Hx1 H1; first by apply IH.
-    by rewrite /halted /= (halted_low H1) in Hx1.
-  - by rewrite equivS; apply: IH.
+    by rewrite /ended /= (ended_low H1) in Hx1.
+  - by rewrite indistS; apply: IH.
 Qed.
 
-(* CH: TODO: allow the two equivalences to vary *)
+(* CH: TODO: allow the two indistinguishability relations to vary *)
 Lemma ssni_llni : ssni -> llni.
 Proof.
   move => SSNI s1 s2 n I1 I2 E12.
@@ -302,15 +305,15 @@ Proof.
   elim: n s1 s2 {I1 I2} E12 n1 n2 Hn => [|n IH] s1 s2 E12 n1 n2 Hn.
   - rewrite leqn0 addn_eq0 in Hn.
     move: Hn => /andP [/eqP -> /eqP ->] /=.
-    exact: equivtb_single E12.
+    exact: indisttb_single E12.
   - case: n1 Hn => [|n1] /= Hn;
     case: n2 Hn => [|n2] /= Hn.
-    + exact: equivtb_single E12.
-    + by case S2: (step s2) => [s2'|] /=; rewrite (equivtb_cons2 _ _ E12) equivtbNt.
-    + by case S1: (step s1) => [s1'|] /=; rewrite (equivtb_cons2 _ _ E12) equivtbtN.
+    + exact: indisttb_single E12.
+    + by case S2: (step s2) => [s2'|] /=; rewrite (indisttb_cons2 _ _ E12) indisttbNt.
+    + by case S1: (step s1) => [s1'|] /=; rewrite (indisttb_cons2 _ _ E12) indisttbtN.
     + case S1: (step s1) => [s1'|]; case S2: (step s2) => [s2'|];
-      try by rewrite (equivtb_cons2 _ _ E12) ?equivtbNt ?equivtbtN.
-      rewrite /equivtb /= -(equiv_low E12).
+      try by rewrite (indisttb_cons2 _ _ E12) ?indisttbNt ?indisttbtN.
+      rewrite /indisttb /= -(indist_low E12).
       have [L|L] := boolP (low s1) => /=.
       * rewrite E12 /=.
         apply IH; first by eapply (ssni_low_low SSNI); eauto.
@@ -323,20 +326,20 @@ Proof.
           eapply ssni_high_low; eauto.
           exact: L.
         - rewrite addnS ltnS in Hn.
-          have E12': equiv s1 s2'.
-          { apply: (equivT E12).
+          have E12': indist s1 s2'.
+          { apply: (indistT E12).
             eapply ssni_high_high; eauto.
-            by rewrite /high /= -(equiv_low E12). }
+            by rewrite /high /= -(indist_low E12). }
           move: IH => /(_ s1 s2' E12' _ _ Hn).
-          by rewrite /equivtb /= S1 /= (negbTE L).
+          by rewrite /indisttb /= S1 /= (negbTE L).
         - rewrite addSn ltnS in Hn.
-          have E12': equiv s1' s2.
-          { apply: (equivT _ E12).
-            rewrite equivS.
+          have E12': indist s1' s2.
+          { apply: (indistT _ E12).
+            rewrite indistS.
             eapply ssni_high_high; eauto. }
           move: IH => /(_ s1' s2 E12' _ _ Hn).
-          rewrite (equiv_low E12) in L.
-          by rewrite /equivtb /= S2 /= (negbTE L).
+          rewrite (indist_low E12) in L.
+          by rewrite /indisttb /= S2 /= (negbTE L).
 Qed.
 
 End Everything.
