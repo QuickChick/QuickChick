4c346f1862a5727e717ac5d5db4b990166c90f99
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2014-08-30 18:40:52 -0400

    EqType instance for instructions.

--- ifc/extended/Instructions.v
+++ ifc/extended/Instructions.v
@@ -12,6 +12,8 @@ Inductive BinOpT : Type :=
 | BFlowsTo
 | BEq.
 
+Scheme Equality for BinOpT.
+
 Section Instr.
 
 Context {Label : Type}.
@@ -120,3 +122,43 @@ Definition opcode_of_instr (i : Instr) : option OpCode :=
 end.
 
 End Instr.
+
+Require Import ssreflect ssrbool eqtype.
+Require Import Utils.
+
+Lemma BinOpT_beqP : Equality.axiom BinOpT_beq.
+Proof. admit. Qed.
+
+Definition BinOpT_eqMixin := EqMixin BinOpT_beqP.
+Canonical BinOpT_eqType := Eval hnf in EqType _ BinOpT_eqMixin.
+
+Definition instr_eqb (T : eqType) (i1 i2 : @Instr T) : bool :=
+  match i1, i2 with
+  | Put n1 r1, Put n2 r2 => [&& n1 == n2 & r1 == r2]
+  | Mov r11 r12, Mov r21 r22 => [&& r11 == r21 & r12 == r22]
+  | Load r11 r12, Load r21 r22 => [&& r11 == r21 & r12 == r22]
+  | Store r11 r12, Store r21 r22 => [&& r11 == r21 & r12 == r22]
+  | Write r11 r12, Write r21 r22 => [&& r11 == r21 & r12 == r22]
+  | BinOp o1 r11 r12 r13, BinOp o2 r21 r22 r23 => [&& o1 == o2, r11 == r21, r12 == r22 & r13 == r23]
+  | Nop, Nop => true
+  | Halt, Halt => true
+  | Jump r1, Jump r2 => r1 == r2
+  | BNZ o1 r1, BNZ o2 r2 => [&& o1 == o2 & r1 == r2]
+  | BCall r11 r12 r13, BCall r21 r22 r23 => [&& r11 == r21, r12 == r22 & r13 == r23]
+  | BRet, BRet => true
+  | Lab r11 r12, Lab r21 r22 => [&& r11 == r21 & r12 == r22]
+  | PcLab r1, PcLab r2 => r1 == r2
+  | PutLab l1 r1, PutLab l2 r2 => [&& l1 == l2 & r1 == r2]
+  | Alloc r11 r12 r13, Alloc r21 r22 r23 => [&& r11 == r21, r12 == r22 & r13 == r23]
+  | PGetOff r11 r12, PGetOff r21 r22 => [&& r11 == r21 & r12 == r22]
+  | PSetOff r11 r12 r13, PSetOff r21 r22 r23 => [&& r11 == r21, r12 == r22 & r13 == r23]
+  | MSize r11 r12, MSize r21 r22 => [&& r11 == r21 & r12 == r22]
+  | MLab r11 r12, MLab r21 r22 => [&& r11 == r21 & r12 == r22]
+  | _, _ => false
+  end.
+
+Lemma instr_eqbP T : Equality.axiom (instr_eqb T).
+Proof. admit. Qed.
+
+Definition instr_eqMixin T := EqMixin (instr_eqbP T).
+Canonical instr_eqType T := Eval hnf in EqType _ (instr_eqMixin T).
