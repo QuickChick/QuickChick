a6e3d5c114ce7f2e827d957c4cb13a46ce414f15
Catalin Hritcu <catalin.hritcu@gmail.com>
2014-09-20 19:51:58 +0200

    Set of outcomes semantics for generators

--- src/Gen.v
+++ src/Gen.v
@@ -8,6 +8,15 @@ Set Implicit Arguments.
 Inductive Gen (A : Type) : Type :=
   | MkGen : (RandomGen -> nat -> A) -> Gen A.
 
+(* Set of outcomes semantics for generators *)
+Require Import Ensembles.
+
+Definition unGen {A : Type} (g : Gen A) : RandomGen -> nat -> A :=
+  match g with MkGen f => f end.
+
+Definition semGen {A : Type} (g : Gen A) : Ensemble A :=
+  fun a => exists seed, exists size, (unGen g) seed size = a.
+
 Definition fmapG {A B : Type} (f : A -> B) (g : Gen A) : Gen B :=
   match g with
     | MkGen h => MkGen (fun r n => f (h r n))
@@ -152,5 +161,3 @@ Instance realGen : GenMonad Gen :=
     suchThatMaybe := @suchThatMaybeG;
     promote := @promoteG
   }.
-
-
