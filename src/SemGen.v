bc593c7ef43cb9ba6ba25b8a8eda4d72bb2ce8c3
Catalin Hritcu <catalin.hritcu@gmail.com>
2014-09-21 10:41:31 +0200

    Some refactoring

--- src/SemGen.v
+++ src/SemGen.v
@@ -7,25 +7,29 @@ Set Implicit Arguments.
 (* Set of outcomes semantics for generators *)
 Require Import Ensembles.
 
-Definition unGen {A : Type} (g : Gen A) : RandomGen -> nat -> A :=
-  match g with MkGen f => f end.
+(* also fixing counterintuitive order of arguments *)
+Definition unGen {A : Type} (g : Gen A) : nat -> RandomGen -> A :=
+  match g with MkGen f => fun n r => f r n end.
+
+Definition semSize {A : Type} (g : RandomGen -> A) : Ensemble A :=
+  fun a => exists seed, g seed = a.
 
 Definition semGen {A : Type} (g : Gen A) : Ensemble A :=
-  fun a => exists size, exists seed, (unGen g) seed size = a.
+  fun a => exists size, semSize ((unGen g) size) a.
 
 (* Equivalence on sets of outcomes *)
 Definition set_eq {A} (m1 m2 : Ensemble A) :=
   forall A, m1 A <-> m2 A.
 
-Infix "<-->" := set_eq (at level 70, no associativity) : pred_scope.
+Infix "<-->" := set_eq (at level 70, no associativity) : sem_gen_scope.
 
 (* the set f outcomes m1 is a subset of m2 *)
 Definition set_incl {A} (m1 m2 : Ensemble A) :=
   forall A, m1 A -> m2 A.
 
-Infix "-->" := set_incl (at level 70, no associativity) : pred_scope.
+Infix "-->" := set_incl (at level 70, no associativity) : sem_gen_scope.
 
-Open Scope pred_scope.
+Open Scope sem_gen_scope.
 
 Axiom randomSeedInhabitant : RandomGen.
 
@@ -43,35 +47,34 @@ Qed.
 Axiom rndSplitAssumption :
   forall s1 s2 : RandomGen, exists s, rndSplit s = (s1,s2).
 
-(* We will clearly need to reason about sizes:
+(* We clearly need to reason about sizes:
    generators need to be size-monotonous functions *)
-(* Currently can't assume this of all generators,
-   but this will probably need to be part of Gen
-   (sigma type? this would break the non-intrusiveness of our solution)
-   / or of some type class (Arbitrary? a super class if it so that
-   users can choose how much they want to prove) *)
-Axiom genSize : forall A (a : A) g seed1 size1 size2,
+(* Went for the most explicit way of doing this,
+   could later try to use type classes to beautify/automate things *)
+Definition sizeMonGen {A : Type} (g : Gen A) := forall a size1 size2,
   (size1 <= size2)%coq_nat ->
-  (unGen g) seed1 size1 = a ->
-  exists seed2, (unGen g) seed2 size2 = a.
+  semSize ((unGen g) size1) a ->
+  semSize ((unGen g) size2) a.
 
 Lemma semBind : forall A B (g : Gen A) (f : A -> Gen B),
+  sizeMonGen g ->
+  (forall a, sizeMonGen (f a)) ->
   semGen (bindG g f) <--> fun b => exists a, (semGen g) a /\ (semGen (f a)) b.
 Proof.
-  move => A B g f b. rewrite /semGen /bindG => /=. split.
-  - case : g => [g] /= [size [seed H]]. move : H.
+  move => A B g f MonG MonF b. rewrite /semGen /bindG => /=. split.
+  - move : MonG. case : g => [g] MonG /= [size [seed H]]. move : H.
     case (rndSplit seed) => [seed1 seed2].
     exists (g seed1 size). split; do 2 eexists. reflexivity.
     rewrite <- H. case (f (g seed1 size)). reflexivity.
-  - move => [a [[size1 [seed1 H1]] [size2 [seed2 H2]]]].
+  - move => [a [[size1 H1] [size2 H2]]].
     assert (Hs1 : (size1 <= max size1 size2)%coq_nat) by apply Max.le_max_l.
-    case (genSize _ _ Hs1 H1) => [seed1' H1'].
+    case (MonG _ _ _ Hs1 H1) => [seed1' H1'].
     assert (Hs2 : (size2 <= max size1 size2)%coq_nat) by apply Max.le_max_r.
-    case (genSize _ _ Hs2 H2) => [seed2' H2'].
+    case (MonF _ _ _ _ Hs2 H2) => [seed2' H2'].
     eexists (max size1 size2). clear H1 H2.
     case (rndSplitAssumption seed1' seed2') => [seed Hs].
     exists seed.
-    move : H1' H2'. case : g => [g] /= H1' H2'.
+    move : H1' H2'. move : MonG. case : g => [g] MonG /= H1' H2'.
     rewrite Hs. rewrite H1'. move : H2'. by case (f a).
 Qed.
 
@@ -96,7 +99,7 @@ Lemma semSized : forall A (f : nat -> Gen A),
 Proof.
   move => A f Mon a. rewrite /semGen /sizedG => /=. split.
   - move => [size [seed H]]. exists size. move : H.
-    case (f size) => g H. by eauto.
+    case (f size) => g H. rewrite /semSize. by eauto.
   - move => [n [size [seed H]]].
     exists (max n size).
     admit. (* Hopefully this follows from something like Mon *)
