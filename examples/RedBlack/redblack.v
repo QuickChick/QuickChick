69d95c91eed20be5bfbf014fff1650ab6a14f183
Catalin Hritcu <catalin.hritcu@gmail.com>
2014-10-19 15:18:54 +0200

    [minor] normalized whitespace

--- examples/RedBlack/redblack.v
+++ examples/RedBlack/redblack.v
@@ -1,13 +1,13 @@
 Require Import ssreflect ssrnat ssrbool eqtype.
 
-(* Formalization inspired from 
+(* Formalization inspired from
    https://www.cs.princeton.edu/~appel/papers/redblack.pdf *)
 
 Inductive color := Red | Black.
 
-Inductive tree := 
+Inductive tree :=
   | Leaf : tree
-  | Node : color -> tree -> nat -> tree -> tree. 
+  | Node : color -> tree -> nat -> tree -> tree.
 
 
 (* RedBlack invariant *)
@@ -24,34 +24,34 @@ Inductive is_redblack : tree -> color -> nat -> Prop :=
 
 (* Boolean *)
 Fixpoint black_height_bool (t: tree) : option nat :=
-  match t with 
+  match t with
     | Leaf => Some 0
     | Node c tl _ tr =>
       let h1 := black_height_bool tl in
       let h2 := black_height_bool tr in
       match h1, h2 with
-        | Some n1, Some n2 => 
-          if n1 == n2 then 
-            match c with 
+        | Some n1, Some n2 =>
+          if n1 == n2 then
+            match c with
               | Black => Some (S n1)
               | Red => Some n1
             end
           else None
-        | _, _ => None 
+        | _, _ => None
       end
   end.
 
 Fixpoint is_redblack_bool (t : tree) (c: color) : bool  :=
-  match t with 
+  match t with
     | Leaf => true
     | Node c' tl _ tr =>
       match c' with
-        | Black => 
+        | Black =>
           (black_height_bool tl == black_height_bool tr) &&
           is_redblack_bool tl Black && is_redblack_bool tr Black
-        | Red =>           
+        | Red =>
           match c with
-            | Black => 
+            | Black =>
               (black_height_bool tl == black_height_bool tr) &&
               is_redblack_bool tl Red && is_redblack_bool tr Red
             | Red => false
@@ -59,7 +59,7 @@ Fixpoint is_redblack_bool (t : tree) (c: color) : bool  :=
       end
   end.
 
-Lemma has_black_height : 
+Lemma has_black_height :
   forall t c, is_redblack_bool t c -> exists n, black_height_bool t = Some n.
 Proof.
   move=> t. induction t; move => c' Hdec; first by exists 0.
@@ -72,64 +72,63 @@ Proof.
 Qed.
 
 Lemma is_redblackP :
-  forall (t : tree) (c : color) n, 
-    reflect (is_redblack t c n) 
+  forall (t : tree) (c : color) n,
+    reflect (is_redblack t c n)
             (is_redblack_bool t c && (black_height_bool t == Some n)).
 Proof.
-  move => t c n. 
-  apply (@iffP (is_redblack_bool t c && (black_height_bool t == Some n))); 
+  move => t c n.
+  apply (@iffP (is_redblack_bool t c && (black_height_bool t == Some n)));
     first by apply/idP.
-  - move: c n. induction t; intros c' n' Hrb.   
-    + move/andP : Hrb => [_ /= /eqP [Hn]]; subst. by constructor. 
-    + move/andP : Hrb => [Hdec /eqP Hheight]. 
+  - move: c n. induction t; intros c' n' Hrb.
+    + move/andP : Hrb => [_ /= /eqP [Hn]]; subst. by constructor.
+    + move/andP : Hrb => [Hdec /eqP Hheight].
       destruct c'; destruct c; simpl in *; (try by discriminate);
       move/andP: Hdec => [/andP [/eqP Heq  Ht1]  Ht2];
       remember (black_height_bool t1) as h1;
       remember (black_height_bool t2) as h2;
       destruct h1, h2; (try discriminate);
-      inversion Heq; subst; rewrite eq_refl in Hheight; 
+      inversion Heq; subst; rewrite eq_refl in Hheight;
       inversion Hheight; subst;
       constructor;
       (try by (apply IHt1; apply/andP; split => //));
-      by (apply IHt2; apply/andP; split => //).  
+      by (apply IHt2; apply/andP; split => //).
   - move=> Hrb. induction Hrb; (try by reflexivity);
     move/andP: IHHrb1 => [Htl /eqP Hhtl];
-    move/andP: IHHrb2 => [Htr /eqP Hhtr]; subst; simpl; 
+    move/andP: IHHrb2 => [Htr /eqP Hhtr]; subst; simpl;
     rewrite Hhtl Hhtr; repeat (apply/andP; split => //);
     by rewrite eq_refl.
 Qed.
-    
-    
+
 Lemma is_redblack_exP :
-  forall (t : tree) (c : color), 
-    reflect (exists n, is_redblack t c n) 
+  forall (t : tree) (c : color),
+    reflect (exists n, is_redblack t c n)
             (is_redblack_bool t c).
 Proof.
   move=> t c.
-  apply (@iffP (is_redblack_bool t c)); 
+  apply (@iffP (is_redblack_bool t c));
     first by apply/idP.
   - move=> Hrb.  move/has_black_height: (Hrb) => [n Hh]. exists n.
-    apply/is_redblackP. apply/andP; split. exact Hrb. 
-      by apply/eqP. 
-  - by move => [n /is_redblackP/andP [Hrb _]].   
+    apply/is_redblackP. apply/andP; split. exact Hrb.
+      by apply/eqP.
+  - by move => [n /is_redblackP/andP [Hrb _]].
 Qed.
 
 
 (* insertion *)
 
 Definition balance rb t1 k t2 :=
-  match rb with 
+  match rb with
     | Red => Node Red t1 k t2
-    | _ => 
-      match t1 with 
-        | Node Red (Node Red a x b) y c => 
+    | _ =>
+      match t1 with
+        | Node Red (Node Red a x b) y c =>
           Node Red (Node Black a x b) y (Node Black c k t2)
-        | Node Red a x (Node Red b y c) => 
+        | Node Red a x (Node Red b y c) =>
           Node Red (Node Black a x b) y (Node Black c k t2)
-        | a => match t2 with 
-                 | Node Red (Node Red b y c) z d =>  
+        | a => match t2 with
+                 | Node Red (Node Red b y c) z d =>
                    Node Red (Node Black t1 k b) y (Node Black c z d)
-                 | Node Red b y (Node Red c z d) => 
+                 | Node Red b y (Node Red c z d) =>
                    Node Red (Node Black t1 k b) y (Node Black c z d)
                  | _ => Node Black t1 k t2
                end
@@ -137,15 +136,15 @@ Definition balance rb t1 k t2 :=
   end.
 
 Fixpoint ins x s :=
-  match s with 
+  match s with
     | Leaf => Node Red Leaf x Leaf
     | Node c a y b => if x < y then balance c (ins x a) y b
                       else if y < x then balance c a y (ins x b)
                            else Node c a x b
   end.
 
-Definition makeBlack t := 
-  match t with 
+Definition makeBlack t :=
+  match t with
     | Leaf => Leaf
     | Node _ a x b => Node Black a x b
   end.
@@ -158,7 +157,7 @@ Definition insert_preserves_redblack :=
                 exists h', is_redblack (insert x s) Red h'.
 
 Definition insert_preserves_redblack_bool :=
-  forall x s, is_redblack_bool s Red -> is_redblack_bool (insert x s) Red. 
+  forall x s, is_redblack_bool s Red -> is_redblack_bool (insert x s) Red.
 
 Lemma insert_preserves_redblack_equiv:
   insert_preserves_redblack <-> insert_preserves_redblack_bool.
--- examples/RedBlack/testing.v
+++ examples/RedBlack/testing.v
@@ -14,13 +14,13 @@ Open Scope Checker_scope.
 Fixpoint showColor (c : color) :=
   match c with
     | Red => "Red "
-    | Black => "Black " 
+    | Black => "Black "
   end.
 
 Fixpoint RBTree_to_string (t : tree) :=
   match t with
     | Leaf => "Leaf"
-    | Node c l x r => "Node " ++ showColor c  ++ show x 
+    | Node c l x r => "Node " ++ showColor c  ++ show x
                             ++ " ( " ++ RBTree_to_string l ++ " ) "
                             ++ " ( " ++ RBTree_to_string r ++ " ) "
   end.
@@ -30,55 +30,55 @@ Instance showRBTree {A : Type} `{_ : Show A} : Show tree :=
     show := RBTree_to_string
   |}.
 
- 
+
 Section Generators.
   Context {Gen : Type -> Type}
           {H: GenMonad Gen}.
 
-  
+
   Definition genColor := elements Red [Red; Black].
-  
+
   Fixpoint genRBTree_height (h : nat) (c : color) :=
     match h with
-      | 0 => 
-        match c with 
+      | 0 =>
+        match c with
           | Red => returnGen Leaf
           | Black => oneof (returnGen Leaf)
-                           [returnGen Leaf; 
+                           [returnGen Leaf;
                              bindGen arbitraryNat (fun n =>
                              returnGen (Node Red Leaf n Leaf))]
         end
-      | S h => 
-        match c with 
-          | Red => 
+      | S h =>
+        match c with
+          | Red =>
             bindGen (genRBTree_height h Black) (fun t1 =>
-            bindGen (genRBTree_height h Black) (fun t2 => 
-            bindGen arbitraryNat (fun n => 
+            bindGen (genRBTree_height h Black) (fun t2 =>
+            bindGen arbitraryNat (fun n =>
             returnGen (Node Black t1 n t2))))
-          | Black => 
+          | Black =>
             bindGen genColor (fun c' =>
-            match c' with 
+            match c' with
               | Red =>
                 bindGen (genRBTree_height h Black) (fun tl1 =>
-                bindGen (genRBTree_height h Black) (fun tl2 => 
+                bindGen (genRBTree_height h Black) (fun tl2 =>
                 bindGen (genRBTree_height h Black) (fun tr1 =>
                 bindGen (genRBTree_height h Black) (fun tr2 =>
-                bindGen arbitraryNat (fun n => 
-                bindGen arbitraryNat (fun nl => 
-                bindGen arbitraryNat (fun nr => 
-                returnGen (Node Red (Node Black tl1 nl tr1) n 
+                bindGen arbitraryNat (fun n =>
+                bindGen arbitraryNat (fun nl =>
+                bindGen arbitraryNat (fun nr =>
+                returnGen (Node Red (Node Black tl1 nl tr1) n
                                 (Node Black tl2 nr tr2)))))))))
               | Black =>
                 bindGen (genRBTree_height h Black) (fun t1 =>
-                bindGen (genRBTree_height h Black) (fun t2 => 
-                bindGen arbitraryNat (fun n => 
+                bindGen (genRBTree_height h Black) (fun t2 =>
+                bindGen arbitraryNat (fun n =>
                 returnGen (Node Black t1 n t2))))
             end)
         end
     end.
 
   Definition genRBTree := sized (fun h => genRBTree_height h Red).
-  
+
 End Generators.
 
 Section Checker.
@@ -87,8 +87,8 @@ Section Checker.
 
   Definition insert_is_redblack_checker : Gen QProp :=
     forAll arbitraryNat (fun n =>
-    (forAll genRBTree (fun t => 
-      (is_redblack_bool t Red ==> 
+    (forAll genRBTree (fun t =>
+      (is_redblack_bool t Red ==>
        is_redblack_bool (insert n t) Red) : Gen QProp)) : Gen QProp).
 
 End Checker.
@@ -111,7 +111,7 @@ Lemma genColor_correct:
   genColor <--> (fun _ => True).
 Proof.
   rewrite /genColor. intros c. rewrite elements_equiv.
-  split => // _. left. 
+  split => // _. left.
   destruct c;  by [ constructor | constructor(constructor)].
 Qed.
 
@@ -120,12 +120,12 @@ Lemma genRBTree_height_correct:
   forall c h,
     (genRBTree_height h c) <--> (fun t => is_redblack t c h).
 Proof.
-  move => c h. move : c. induction h as [| h]; intros c' t. 
+  move => c h. move : c. induction h as [| h]; intros c' t.
   { rewrite /genRBTree_height. split.
-    - destruct c'. 
+    - destruct c'.
       + rewrite returnGen_def. by move => <-; constructor.
       + rewrite oneof_equiv. move => [[gen [H Hgen]] | [// H Hret]].
-        inversion H as [HIn | HIn]; subst. 
+        inversion H as [HIn | HIn]; subst.
         * rewrite returnGen_def in Hgen; subst. by constructor.
         * inversion HIn as [HIn' |  HIn'] => //; subst.
         move: Hgen => [n [Hn Hret]]. rewrite returnGen_def in Hret; subst.
@@ -134,17 +134,17 @@ Proof.
       + destruct c' => //. rewrite oneof_equiv. left.
         eexists. by split; [by apply in_eq |].
       + rewrite oneof_equiv. left. eexists.
-        split. by constructor(apply in_eq). 
+        split. by constructor(apply in_eq).
         eexists. split; first by apply arbNat_correct.
         inversion H0; subst. inversion H1; subst.
         reflexivity. }
   { split.
-    - destruct c'. 
+    - destruct c'.
       + move => [tl [/IHh Hgentl [tr [/IHh Hgentr [n [_ H]]]]]].
         rewrite returnGen_def in H; subst.
         by constructor => //.
-      + move => [[] [Hcol Hgen]]. 
-        * move : Hgen => 
+      + move => [[] [Hcol Hgen]].
+        * move : Hgen =>
           [tl1 [/IHh Htl1 [tl2 [/IHh Htl2 [tr1 [/IHh Htr1 [tr2 [/IHh Htr2 H]]]]]]]].
           move : H => [n [_ [nl [_ [nr [_ H]]]]]].
           rewrite returnGen_def in H; subst.
@@ -152,11 +152,11 @@ Proof.
        * move : Hgen => [tl [/IHh Hgentl [tr [/IHh Hgentr [n [_ H]]]]]].
          rewrite returnGen_def in H; subst.
          constructor => //.
-    - move => H. inversion H as [| n tl tr h' Hrbl Hrbr | 
-                                   c n tl tr h' Hrbl Hrbr]; subst. 
-      + inversion Hrbl as [| |c n1 tl1 tr1 h1 Hrbl1 Hrbr1]; subst. 
+    - move => H. inversion H as [| n tl tr h' Hrbl Hrbr |
+                                   c n tl tr h' Hrbl Hrbr]; subst.
+      + inversion Hrbl as [| |c n1 tl1 tr1 h1 Hrbl1 Hrbr1]; subst.
         inversion Hrbr as [| |c n2 tl2 tr2 h2 Hrbl2 Hrbr2]; subst.
-        exists Red. split; first by apply genColor_correct. 
+        exists Red. split; first by apply genColor_correct.
         exists tl1. split; first by apply IHh.
         exists tl2. split; first by apply IHh.
         exists tr1. split; first by apply IHh.
@@ -165,11 +165,11 @@ Proof.
         exists n1. split; first by apply arbNat_correct.
         exists n2. split; first by apply arbNat_correct.
         reflexivity.
-      + destruct c'. 
+      + destruct c'.
         * exists tl. split; first by apply IHh.
           exists tr. split; first by apply IHh.
           exists n. split; first by apply arbNat_correct.
-          reflexivity. 
+          reflexivity.
         * exists Black. split; first by apply genColor_correct.
           exists tl. split; first by apply IHh.
           exists tr. split; first by apply IHh.
@@ -181,30 +181,30 @@ Lemma genRBTree_correct:
   genRBTree <--> (fun t => exists h, is_redblack t Red h).
 Proof.
   move => t. split.
-  - rewrite /genRBTree sized_def.  
+  - rewrite /genRBTree sized_def.
     move => [n /genRBTree_height_correct Hgen].
       by exists n.
   - move => [n /genRBTree_height_correct Hgen].
     rewrite /genRBTree sized_def. by exists n.
 Qed.
-    
+
 
 Lemma insert_is_redblack_checker_correct:
   semChecker insert_is_redblack_checker <-> insert_preserves_redblack.
 Proof.
-  rewrite /insert_is_redblack_checker /insert_preserves_redblack semForAll. 
+  rewrite /insert_is_redblack_checker /insert_preserves_redblack semForAll.
   split.
   + move => H x s n Hrb.
-    have /H/semPredQProp/semForAll H' : @arbitraryNat Pred _ x 
+    have /H/semPredQProp/semForAll H' : @arbitraryNat Pred _ x
       by apply arbNat_correct.
-    have /H'/semPredQProp/semImplication H'': @genRBTree Pred _ s 
+    have /H'/semPredQProp/semImplication H'': @genRBTree Pred _ s
       by apply genRBTree_correct; eexists; eassumption.
-    apply/is_redblack_exP. 
-    by move/is_redblack_exP/H''/semBool: (ex_intro _ _ Hrb).  
+    apply/is_redblack_exP.
+    by move/is_redblack_exP/H''/semBool: (ex_intro _ _ Hrb).
   + move=> H a _.
     apply/semPredQProp/semForAll. move => t Hgen.
     apply/semPredQProp/semImplication. move => Hrb.
     apply/semBool. apply/is_redblack_exP.
     move: Hrb => /is_redblack_exP [n Hrb]. eapply H.
-    eassumption. 
+    eassumption.
 Qed.
