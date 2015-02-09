e83ceac2ac872828919da30b7f715d26afbea937
Leonidas Lampropoulos <llamp@seas.upenn.edu>
2015-02-08 20:37:16 -0500

    CP in CoArbitrary proofs. Clocking out for the day...

--- src/CoArbitrary.v
+++ src/CoArbitrary.v
@@ -13,58 +13,83 @@ Class CoArbitrary (A : Type) : Type :=
                                                                     coarbitrary y]
   }.
 
-Fixpoint posToPath (p : positive) : SplitPath := 
+Local Open Scope positive.
+Fixpoint posToPathAux (p : positive) : SplitPath := 
   match p with 
-    | xH => [Right]
-    | xI p' => posToPath p' ++ [Left; Right]
-    | xO p' => posToPath p' ++ [Left; Left ]
+    | xH => []
+    | xI p' => posToPathAux p' ++ [Left; Right]
+    | xO p' => posToPathAux p' ++ [Left; Left ]
   end.
+
+Definition posToPath (p : positive) : SplitPath := posToPathAux p ++ [Right].
 
 Instance coArbPos : CoArbitrary positive := 
   {|
     coarbitrary := posToPath
   |}.
 Proof.
-intros.  
-eapply FreeCons.
-+ constructor. 
-+ unfold In. left. eauto.
-+ instantiate (1 := []). simpl.
-  generalize dependent y.
-  induction x.
-  - intros.
-    destruct y.
-    rewrite app_nil_r in H0.
-    simpl in H0.
-    apply app_inv_tail in H0.
-    
+Admitted.
 
-    * compute in H0; simpl in *.
-
-Require Import Recdef.
-Require Import Numbers.Natural.Peano.NPeano.
-Require Import ZArith.
-Function natToPath (n : nat) {measure (fun n => n) n} : SplitPath := 
-  match n with 
-    | O => [Right]
-    | _ => (natToPath (n / 2))
-           ++ (Left :: (if beq_nat (modulo n 2) 0 then Right else Left) :: [])
+Function rangeNat (p : nat) : list nat :=
+  match p with 
+    | O => [] 
+    | S n' => p :: (rangeNat n')
   end.
-Proof.
-intros; subst; apply Nat.div_lt; [apply lt_0_Sn | ]; auto.
-Defined. 
 
-Instance coArbNat : CoArbitrary nat :=
-  {|
-    coarbitrary := natToPath
-  |}.
-Proof.
-intros.  
-eapply FreeCons.
-+ constructor. 
-+ unfold In. left. eauto.
-+ instantiate (1 := []). admit.
-Qed.
+Definition rangePos (p : positive) : list positive := 
+  map Pos.of_nat (rangeNat (Pos.to_nat p)).
+
+Lemma rangeNatLt : forall n m, In m (rangeNat n) -> lt m (S n).
+  induction n; intros.
+  + inversion H.
+  + inversion H.
+    - subst. unfold lt. apply le_n.
+    - apply IHn in H0.
+      unfold lt in *.
+      apply le_S.
+      auto.
+Qed.    
+
+Lemma rangePosPrefixFree : forall p, PrefixFree (map coarbitrary (rangePos p)).
+  intros.
+  unfold rangePos.
+  induction (Pos.to_nat p).
+  + constructor.
+  + simpl. apply FreeCons; auto.
+    intros.
+    apply in_map_iff in H.
+    clear IHn.
+    inversion H; clear H.
+    inversion H1; clear H1.
+    subst.
+    apply in_map_iff in H2.
+    inversion H2; clear H2.
+    inversion H; clear H.
+    apply rangeNatLt in H2.
+    remember (match n with | O => 1 | S _ => Pos.succ (Pos.of_nat n) end) as m.
+    assert (x <> m). admit.
+    pose proof coarbitraryCorrect x m.
+    apply H3 in H.
+    inversion H.
+    eapply H7.
+    + left; auto.
+    + eauto.
+Qed.    
+
+Theorem coArbComplete : forall (max : positive) (f : positive -> RandomSeed) ,
+                          exists seed, forall p, p <= max -> 
+                            varySeed (coarbitrary p) seed = f p.
+intros.
+pose proof (topLevelSeedTheorem (map coarbitrary (rangePos max)) (fun p => f (rangePosPrefixFree _)).
+
+topLevelSeedTheorem
+     : forall (l : list SplitPath) (f : SplitPath -> RandomSeed),
+       PrefixFree l ->
+       exists s : RandomSeed,
+         forall p : SplitPath, In p l -> varySeed p s = f p
+
+                          
+                          p <= max -> exists seed, fora
 
 Instance arbFun {A B : Type} `{_ : CoArbitrary A} `{_ : Arbitrary B} : Arbitrary (A -> B) :=
   {|
