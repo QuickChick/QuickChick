02abb66476ced488a83dcdd5e8df5fe857119bed
Zoe Paraskevopoulou <zoe.paraskevopoulou@gmail.com>
2017-02-13 18:18:21 -0500

    Correctness for non-size parametric generators; derived by typeclass resolution -- no proof generation required

--- examples/classes.v
+++ examples/classes.v
@@ -72,24 +72,22 @@ Class GenSizeCorrect {A : Type} `{Sized A} (g : nat -> G A) :=
     genSizeCorrect : forall s, semGen (g s) <--> [set x : A | size x <= s ]
   }.
 
-(* Not used so far *)
+(** Monotonicity of size parametric generators *)
 Class ArbitrarySizedMonotonic (A : Type) `{H1 : ArbitrarySized A}
       `{H2 : forall s, SizeMonotonic (arbitrarySize s)}.
 
-(* (** Monotonicity of size parametric generators *) *)
-(* Class ArbitrarySizedMotonic (A : Type) `{ArbitrarySized A} := *)
-(*   { *)
-(*     monotonic : *)
-(*       forall s s1 s2, *)
-(*         s1 <= s2 -> *)
-(*         semGenSize (arbitrarySize s1) s \subset semGenSize (arbitrarySize s2) s *)
-(*   }. *)
-
+(** Monotonicity of size parametric generators v2 *)
+Class ArbitrarySizedSizeMotonic (A : Type) `{ArbitrarySized A} :=
+  {
+    sizeMonotonic :
+      forall s s1 s2,
+        s1 <= s2 ->
+        semGenSize (arbitrarySize s1) s \subset semGenSize (arbitrarySize s2) s
+  }.
 
 (* Correctness of size parametric generators *)
 Class ArbitrarySizedCorrect (A : Type) `{Sized A} `{H1 : ArbitrarySized A}
       `{@GenSizeCorrect _ _ arbitrarySize}.
-
 
 Class ArbitraryMonotonic (A : Type) `{Arbitrary A}
       `{H2 : @SizeMonotonic _ arbitrary}.
@@ -100,13 +98,59 @@ Class GenCorrect (A : Type) (g : G A)  :=
     arbitraryCorrect : semGen g <--> [set : A]
   }.
 
+(* Monotonic and Correct generators *)
 Class ArbitraryMonotonicCorrect (A : Type)
-      `{H1 : Arbitrary A} `{ArbitraryMonotonic A}
-      `{@GenCorrect _ arbitrary}.
-  
+      `{H1 : Arbitrary A}
+      `{ArbitraryMonotonic A} `{@GenCorrect A arbitrary}.
+
+
+(** Once we have instances for size parametric generator, we can derive the instances
+    for the ones that abstract away from sizes *)
+
+Instance ArbitraryFromSized (A : Type) `{ArbitrarySized A} : Arbitrary A.
+Proof.
+  constructor. exact (sized arbitrarySize).
+  exact (shrinkSize).
+Defined.
+
+Instance ArbitraryMonotonicFromSized (A : Type)
+         {H1 : ArbitrarySized A}
+         {H2 : (forall s : nat, SizeMonotonic (arbitrarySize s)) }
+         {H3 : @ArbitrarySizedSizeMotonic A H1} : @SizeMonotonic A arbitrary.
+Proof.
+  constructor. eapply sizedMonotonic.
+  now intros n; eauto with typeclass_instances.
+  intros. edestruct H3. eauto.
+Qed.
+
+Instance ArbitraryCorrectFromSized (A : Type)
+         {H1 : ArbitrarySized A}
+         {Hs : Sized A}
+         {H2 : (forall s : nat, SizeMonotonic (arbitrarySize s)) }
+         {H3 : @ArbitrarySizedSizeMotonic A H1}
+         {H4 : GenSizeCorrect arbitrarySize} : GenCorrect A arbitrary.
+Proof.
+  constructor. unfold arbitrary, ArbitraryFromSized.
+  eapply set_eq_trans.
+  - eapply semSized_alt; eauto with typeclass_instances.
+    destruct H3. eauto.
+  - setoid_rewrite genSizeCorrect.
+    split. intros [n H]. constructor; eauto.
+    intros H. eexists; split; eauto.
+Qed.
+
 (** Foo example *)
 
 (* TODO : prove and move to the appropriate file *)
+
+Lemma nat_set_ind (A : Type) `{Hyp : CanonicalSized A} :
+  forall (P : nat -> set A -> Prop),
+    (P 0 zeroSized) ->
+    (forall (n : nat) s, P n s -> P (n.+1) (succSized s)) ->
+    (forall (n : nat), P n [ set x | size x <= n ]).
+Proof.
+Admitted.
+
 Instance frequencySizeMonotonic_alt 
 : forall {A : Type} (g0 : G A) (lg : seq (nat * G A)),
     SizeMonotonic g0 ->
@@ -114,6 +158,17 @@ Instance frequencySizeMonotonic_alt
     SizeMonotonic (frequency g0 lg).
 Admitted.
 
+Lemma semFreq :
+  forall (A : Type) (ng : nat * G A) (l : seq (nat * G A)),
+    semGen (freq ((fst ng, snd ng) ;; l)) <-->
+    \bigcup_(x in (ng :: l)) semGen x.2.
+Admitted.
+
+Lemma semFreqSize :
+  forall (A : Type) (ng : nat * G A) (l : seq (nat * G A)) (size : nat),
+    semGenSize (freq ((fst ng, snd ng) ;; l)) size <-->
+    \bigcup_(x in (ng :: l)) semGenSize x.2 size.
+Admitted.
 
 Definition monotonicGen {A} (gen : G A) :=
   forall s1 s2 : nat,
@@ -126,33 +181,7 @@ Inductive Foo (A : Type) {B : Type}: Type :=
 | Foo3 : nat -> A -> B -> Foo A -> Foo A -> Foo A
 | Foo4 : Foo A.
 
-DeriveArbitrarySized Foo as "ArbSizedFoo".
-DeriveSized Foo as "SizedFoo". (* Drop params maybe??? *)
-DeriveCanonicalSized Foo as "CanonSizedFoo". (* Drop params maybe??? *)
-
-(* Derives :
-
-Instance arbFooArbitrarySizedGenMonotonic {A B : Type}
-         `{H1 : ArbitraryMonotonic A}
-         `{H2 : ArbitraryMonotonic B}
-         (s : nat)
-        : SizeMonotonic (@arbitrarySize (@Foo A B) arbSizedFoo s).
-
-Needs the arbitrarySized instance to be specified.
-
-*)
-
-DeriveArbitrarySizedMonotonic Foo as "ArbSizedMonFoo" using "ArbSizedFoo".
-
-
-Lemma nat_set_ind (A : Type) `{Hyp : CanonicalSized A} :
-  forall (P : nat -> set A -> Prop),
-    (P 0 zeroSized) ->
-    (forall (n : nat) s, P n s -> P (n.+1) (succSized s)) ->
-    (forall (n : nat), P n [ set x | size x <= n ]).
-Proof.
-Admitted.
-
+(* kinda slow *)
 Lemma singl_set_eq: forall (A : Type) (x : A), [ x ] <--> [ set x ].
 Proof.
   intros A x x'; split; intros H.
@@ -160,71 +189,102 @@ Proof.
   - inv H. now constructor.
 Qed.
 
-Lemma semFreq :
-  forall (A : Type) (ng : nat * G A) (l : seq (nat * G A)),
-    semGen (freq ((fst ng, snd ng) ;; l)) <-->
-    \bigcup_(x in (ng :: l)) semGen x.2.
-Admitted.
 
 Instance ArbNatGenCorrect : GenCorrect nat arbitrary.
 Proof.
   constructor. now apply arbNat_correct.
 Qed.
 
-(* kinda slow *)
+DeriveArbitrarySized Foo as "ArbSizedFoo".
+DeriveSized Foo as "SizedFoo".
+DeriveCanonicalSized Foo as "CanonSizedFoo". (* Drop params maybe??? *)
+DeriveArbitrarySizedMonotonic Foo as "ArbSizedMonFoo" using "ArbSizedFoo".
 DeriveArbitrarySizedCorrect Foo as "ArbCorrMonFoo" using "ArbSizedFoo" and "ArbSizedMonFoo".
 
+Require Import Omega.
 
-Instance FooArbitrarySizedCorrect {A B : Type} `{H1 : Arbitrary A} `{H2 : Arbitrary B}
-         `{H1' : Sized A} `{H2' : Sized B} : (@ArbitrarySizedCorrect (@Foo A B) _ _). 
+Lemma set_incl_trans {A} (s1 s2 s3 : set A) :
+  s1 \subset s2 -> s2 \subset s3 -> s1 \subset s3.
 Proof.
-  constructor. exact arbFooCorrectSized.
+  now firstorder.
 Qed.
 
-Instance ArbitraryFromSized (A : Type) `{ArbitrarySized A} : Arbitrary A.
+Lemma setU_set_incl_compat :
+  forall (T : Type) (s1 s2 s1' s2' : set T),
+    s1 \subset s1' -> s2 \subset s2' -> s1 :|: s2 \subset s1' :|: s2'.
 Proof.
-  constructor. exact (sized arbitrarySize).
-  exact (shrinkSize).
-Defined.
+  now firstorder.
+Qed.
 
-(* Instance ArbitraryMonotonicFromSized (A : Type) *)
-(*          {H1 : ArbitrarySized A} *)
-(*          {H2 : @ArbitrarySizedGenMonotonic A H1} *)
-(*          {H3 : @ArbitrarySizedMotonic A H1} : @ArbitraryMotonic A _. *)
-(* Proof. *)
-(*   constructor. eapply sizedMonotonic. *)
-(*   intros n; eauto with typeclass_instances. *)
-(*   edestruct H3. eauto. *)
-(* Qed. *)
+Lemma incl_bigcupl :
+  forall (T U : Type) (A B : set T) (F : T -> set U),
+    A \subset B -> \bigcup_(x in A) F x \subset \bigcup_(x in B) F x.
+Proof.
+  now firstorder.
+Qed.
 
-(* Instance FooArbitrarySizedM {A B : Type} `{H1 : Arbitrary A} `{H2 : Arbitrary B} *)
-(*          `{H1' : Sized A} `{H2' : Sized B} : @ArbitrarySizedMotonic (@Foo A B) _. *)
-(* Proof. *)
-(*   constructor. intros. *)
-(*   (* we need proof for that *) *)
-(* Admitted. *)
+Lemma incl_bigcupr :
+  forall (T U : Type) (A : set T) (F G : T -> set U),
+    (forall x : T, F x \subset G x) ->
+    \bigcup_(x in A) F x \subset \bigcup_(x in A) G x.
+Proof. 
+  now firstorder. 
+Qed.
 
-(* Instance ArbitraryCorrectFromSized (A : Type) *)
-(*          {H1 : ArbitrarySized A} *)
-(*          {Hs : Sized A} *)
-(*          {H2 : @ArbitrarySizedMotonic A H1} *)
-(*          {H3 : @ArbitrarySizedGenMonotonic A H1} *)
-(*          {H4 : @ArbitrarySizedCorrect A Hs H1} : @ArbitraryCorrect A _. *)
-(* Proof. *)
-(*   constructor. unfold arbitrary, ArbitraryFromSized. *)
-(*   eapply set_eq_trans. *)
-(*   - eapply semSized_alt; eauto with typeclass_instances. *)
-(*     destruct H2. eauto. *)
-(*   - setoid_rewrite arbitrarySizeCorrect. *)
-(*     split. intros [n H]. constructor; eauto. *)
-(*     intros H. eexists; split; eauto. *)
-(* Qed. *)
+Lemma incl_bigcup :
+  forall (T U : Type) (A B : set T) (F G : T -> set U),
+    A \subset B ->
+    (forall x : T, F x \subset G x) ->
+    \bigcup_(x in A) F x \subset \bigcup_(x in B) G x.
+Proof. 
+  now firstorder. 
+Qed.
 
-(* Typeclasses eauto := debug. *)
+Instance ArbSizedSizeMotonicFoo {A B} `{Arbitrary A} `{Arbitrary B} : ArbitrarySizedSizeMotonic (@Foo A B).
+Proof.
+  constructor. intros s s1 s2.
+  revert s1; induction s2; intros s1 Hleq; destruct s1; try ssromega.
+  - simpl. firstorder. (* reflexivity for set_incl *)
+  - unfold arbitrarySize, ArbSizedFoo.
+    rewrite (semFreqSize _ ((1, bindGen arbitrary (fun p0 : A => returnGen (Foo1 A p0))))).
+    admit. 
+  - unfold arbitrarySize, ArbSizedFoo.
+    rewrite !(semFreqSize _ ((1, bindGen arbitrary (fun p0 : A => returnGen (Foo1 A p0))))).
+    rewrite !cons_set_eq.
+    rewrite !bigcup_setU_l.
+    rewrite !bigcup_set1.
+    eapply setU_set_incl_compat.
+    now firstorder.
 
-(* Definition goodArbFoo {A B} `{H1 : Arbitrary A} `{H2 : Arbitrary B} *)
-(*          `{H1' : Sized A} `{H2' : Sized B} : Arbitrary (@Foo A B) := @ArbitraryFromSized (@Foo A B) _. *)
+    eapply setU_set_incl_compat.
+    rewrite !semBindSize.
+    eapply incl_bigcupl.
+    now eauto.
 
-(* Definition correct {A B} `{H1 : Arbitrary A} `{H2 : Arbitrary B} *)
-(*            `{H1' : Sized A} `{H2' : Sized B} := *)
-(*   @arbitraryCorrect (@Foo A B) goodArbFoo _. *)
+    eapply setU_set_incl_compat.
+    rewrite !semBindSize.
+    eapply incl_bigcupr.
+    intros x. rewrite !semBindSize.
+    eapply incl_bigcupr.
+    intros x1. rewrite !semBindSize.
+    eapply incl_bigcupr.
+    intros x2. rewrite !semBindSize.
+    eapply incl_bigcup. eapply IHs2. eassumption.
+    intros x3. rewrite !semBindSize.
+    eapply incl_bigcup. eapply IHs2. eassumption.
+    intros x4. firstorder. (* refl *)
+
+    eapply setU_set_incl_compat.
+    now firstorder.
+    now firstorder.
+Admitted. 
+
+Typeclasses eauto := debug.
+
+(* Typeclass resolution should be able to derive instances for unsized *)
+
+Definition genFoo {A B : Type } `{H1 : Arbitrary A} `{H2 : Arbitrary B}
+           `{H1' : Sized A} `{H2' : Sized B} : G (@Foo A B) := @arbitrary (@Foo A B) _.
+
+Definition corrFoo {A B : Type } `{H1 : ArbitraryMonotonicCorrect A} `{H2 : ArbitraryMonotonicCorrect B}
+  := @arbitraryCorrect (@Foo A B) arbitrary.
