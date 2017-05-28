9c7ddc82e34c38ee4787e6f3da919ac735f2368d
Karl Palmskog <palmskog@gmail.com>
2017-05-27 22:07:08 -0500

    run plugin tests after install

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -8,6 +8,7 @@ opam repo add coq-released https://coq.inria.fr/opam/released
 case $MODE in
   quickchick-plugin)
     opam pin add quickchick-plugin . --yes --verbose
+    make -j4 tests
     ;;
   quickchick-tool)
     opam pin add quickchick-tool . --yes --verbose
