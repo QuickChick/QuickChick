7baab8a4c76db3955eccb8f3ed41b46a3f5c1cc1
Benjamin Pierce <bcpierce00@gmail.com>
2017-06-19 08:09:35 -0400

    Notes on typeclasses, etc.

--- dsss17/TypeClasses.v
+++ dsss17/TypeClasses.v
@@ -1,44 +1,27 @@
-(* see Learn You A Haskell, or Simon PJ, Classes, Jim, But Not as We
-  Know Them — Type Classes in Haskell: What, Why, and Whither (video
-  from OPLSS?)
-
-  good background http://learnyouahaskell.com/types-and-typeclasses
-    and Simon PJ, Classes, Jim, But Not as We Know Them — Type Classes
-    in Haskell: What, Why, and Whither (video from OPLSS?)
-
-  By "tutorial on basic Haskell type classes" I am assuming that you
-  mean something which explains what a type class is, how to create
-  and use them, and so on? (As opposed to, say, something which
-  introduces some of the particular type classes in the standard
-  library?)  I suppose you might be able to use my lecture notes as a
-  starting point:
-  http://www.seas.upenn.edu/~cis194/spring13/lectures/05-type-classes.html
-  .  I don't know if it's good but it's certainly short.  The Haskell
-  wikibook is also usually pretty good:
-  https://en.wikibooks.org/wiki/Haskell/Classes_and_types , though on
-  skimming it now I think it probably spends too much time on
-  incidental details with not enough examples.  All the other basic
-  type class tutorials that I know of are in textbooks that I haven't
-  read so I can't vouch for them.
-
-Reference manual chapter:
-  https://coq.inria.fr/refman/Reference-Manual023.html "Gentle"
-  Introduction:
-  http://www.labri.fr/perso/casteran/CoqArt/TypeClassesTut/typeclassestut.pdf
-  StackOverflow:
-  https://stackoverflow.com/questions/29872260/coq-typeclasses-vs-dependent-records
-  Sozeau slides:
-  https://www.cis.upenn.edu/~bcpierce/courses/670Fall12/slides.pdf
-
-  an example or two of how what are compiled into (dictionary passing)
-
-  advice about advantages and disadvantages (and alternatives?)
- *)
-
 Require Import Coq.Strings.String.
 Require Import Coq.Arith.Arith.
 Local Open Scope string.
 
+(* ################################################################# *)
+(** * Basics *)
+
+(** ** First Example: The [Show] Typeclass *)
+
+(* Motivation: Need to be able to convert lots of things to strings... 
+     - show : A -> String
+
+   Similar examples:
+     - sort : list A -> list A
+     - eqb : A -> A -> bool      (is that the right name?)
+     - + : A -> A -> A
+     - serialize : A -> BitString
+     - hash : A -> Int
+     - etc., etc.
+
+  Coq adopts (and adapts) Haskell's notion of _typeclasses_ for this.
+*)
+
+(* Class declaration: *)
 Class Show (A : Type) : Type :=
 {
   show : A -> string
@@ -47,28 +30,18 @@ Class Show (A : Type) : Type :=
 Check Show.  (* explain the output! *)
 Check show.  (* explain the output! *)
 
+(* Instance declaration: *)
 Instance showBool : Show bool :=
-{|
-  show := fun b:bool => if b then "true" else "false"
-|}.
+  {|
+    show := fun b:bool => if b then "true" else "false"
+  |}.
 
-Instance showString : Show string :=
-{|
-  show := fun s:string => """" ++ s ++ """"
-|}.
+Compute (show true).
 
 Definition natToDigit (n : nat) : string :=
   match n with
-    | 0 => "0"
-    | 1 => "1"
-    | 2 => "2"
-    | 3 => "3"
-    | 4 => "4"
-    | 5 => "5"
-    | 6 => "6"
-    | 7 => "7"
-    | 8 => "8"
-    | _ => "9"
+    | 0 => "0" | 1 => "1" | 2 => "2" | 3 => "3" | 4 => "4" | 5 => "5"
+    | 6 => "6" | 7 => "7" | 8 => "8" | _ => "9"
   end.
 
 Fixpoint writeNatAux (time n : nat) (acc : string) : string :=
@@ -86,32 +59,156 @@ Definition string_of_nat (n : nat) : string :=
   writeNatAux n n "".
 
 Instance showNat : Show nat :=
-{|
-  show := string_of_nat
-|}.
+  {|
+    show := string_of_nat
+  |}.
 
 Compute (show 42).
 
-(* Can eliminate _, right? *)
-Instance showPair {A B : Type} `{_ : Show A} `{_ : Show B} : Show (A * B) :=
-{|
-  show p := match p with (a,b) => ("(" ++ show a ++ "," ++  show b ++ ")") end
-|}.
+Instance showString : Show string :=
+  {|
+    show := fun s:string => """" ++ s ++ """"
+  |}.
 
-(* Explain: What does the ` syntax mean? *)
+Compute (show true).
 
-(* Do we want to show them that leaving out some fields causes Coq to
-   go into "Proof mode" for the others?  I guess not unless we need
-   this. *)
+(* Exercise: Write show instance for pairs of a nat and a bool. *)
 
-(* now show a parameterized type class (what's the simplest example?) *)
+(* One downside of using typeclasses is that error messages get more
+   puzzling (sometimes substantially so).  Here is a common one. *)
+(*
+Inductive foo :=
+  Foo : nat -> foo.
 
-(* now show the definition of Dep *)
+Definition showMyList :=
+  show (Foo 43).
+
+===> 
+      Error: Unable to satisfy the following constraints:
+      ?Show : "Show foo"
+
+To fix it, we just have to define a new instance... (do it, or ask them to)
+*)
+
+(* What if we *)
+
+Definition showFancy {A : Type} `{Show A} (a : A): string :=
+  "((((((((((( " ++ show a ++ " )))))))))))".
+
+Compute (showFancy true).
 
 
+(* Explain: What does the ` syntax mean?  Note that we can name the
+   parameter if we want -- i.e., `{Show A} is just the same as `{_ :
+   Show A}. Explain when you'd need to do this, or better yet explain
+   it below and put a pointer here. *)
+
+(* Recommended exercise: What happens if we forget the class
+   constraint?  Try deleting it and see what happens.  Do you
+   understand why? *)
+
+(* MAYBE: Show one more simple typeclass, perhaps equality *)
+
+(* Remark for newcomers: the name "typeclasses" may sound a bit like
+   "classes" from OOP.  But this is misleading.  A better analogy is
+   actually with _interfaces_ from languages like Java.  But best of
+   all is to set aside OO preconceptions and try to approach the
+   situation with an open mind. *)
+
+(* ---------------------------------------------------------------- *)
+(** Internals *)
+
+(* Explain briefly what a typeclass actually translates into. *)
+
+(* ---------------------------------------------------------------- *)
+(** Parameterized Instances: New Typeclasses from Old *)
+
+Instance showPair {A B : Type} `{Show A} `{Show B} : Show (A * B) :=
+  {|
+    show p :=
+      match p with (a,b) =>
+      | ("(" ++ show a ++ "," ++ show b ++ ")")
+      end
+  |}.
+
+(* Exercise: Write show instances for options and lists *)
+
+(* Exercise?: Define a Ord typeclass, with instances for nat, bool,
+   pairs, and lists. *)
+
+(** Typeclasses and Proofs *)
+
+(* E.g. maybe Matthieu's EqDec example.  (And then we could replace
+   Show above by Eq.) *)
+
+(*
+################################################################# *)
+(** * Some Useful Typeclasses *)
+
+(* MORE: Equality or equivalence?  *)
 
 (* ----------------------------------------------------------------- *)
-(* More stuff... *)
+(** ** [Dep] *)
+
+(* ----------------------------------------------------------------- *)
+(** ** [Monad] *)
+
+(* Mention ext-lib, but not sure whether it's a good idea to actually
+   go into the details... Might be a good case study. *)
+
+(* ----------------------------------------------------------------- *)
+(** ** Others *)
+
+(* Enumerate some of the interesting ones in the standard
+   library... E.g., Functor (is there one??)?  Monoid?  See what else
+   Matthieu likes... *)
+
+(*
+################################################################# *)
+(** * Pragmatics *)
+
+(* Advice about how to use typeclasses in Coq.  How to avoid various
+   pitfalls and gotchas.  How to debug. *)
+
+(* Alternatives.  Choosing among typeclasses, modules, dependent
+   records, canonical instances.  Pointers to where to read about all
+   these. *)
+
+(*
+################################################################# *)
+(** * Further Reading *)
+
+(* Origins: In Haskell, Wadler & Blott, POPL’89.  In Isabelle, Nipkow
+& Snelting, FPCA’91.  In Coq: Sozeau
+*)
+
+(* Typeclasses in Haskell:
+     - https://en.wikibooks.org/wiki/Haskell/Classes_and_types
+          - maybe a bit too low-level, not enough examples
+     - http://learnyouahaskell.com/types-and-typeclasses and
+       http://learnyouahaskell.com/making-our-own-types-and-typeclasses
+     - Simon PJ, Classes, Jim, But Not as We Know Them — Type Classes
+       in Haskell: What, Why, and Whither (video from OPLSS?)
+
+  Typeclasses in Coq:
+     - Reference manual chapter:
+           https://coq.inria.fr/refman/Reference-Manual023.html
+     - "Gentle" Introduction:
+           http://www.labri.fr/perso/casteran/CoqArt/TypeClassesTut/typeclassestut.pdf
+     - StackOverflow:
+           https://stackoverflow.com/questions/29872260/coq-typeclasses-vs-dependent-records
+     - Sozeau slides:
+           https://www.cis.upenn.edu/~bcpierce/courses/670Fall12/slides.pdf
+*)
+
+(* HIDE *)
+(*
+################################################################# *)
+(*
+################################################################# *)
+(*
+################################################################# *)
+(* More Ideas...  *)
 
 (* What does "Polymorphic Instance" mean? *)
 
@@ -163,5 +260,7 @@ EXERCISE: Find a different way of making instance inference diverge.
 
 (* Hint: If confused, you can look at the *coq* buffer. That's where
 most debug messages appear if they don't appear in the *response*
-buffer.
+buffer.  (What's a typical example of this? *)
 *)
+
+(* /HIDE *)
