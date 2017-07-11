481d28c801e5b8e7792e85d9663d92f86c95b9dc
Leonidas Lampropoulos <llamp@seas.upenn.edu>
2017-07-11 16:52:49 -0400

    Exposing discards

--- src/Test.v
+++ src/Test.v
@@ -121,7 +121,7 @@ Definition summary (st : State) : list (string * nat) :=
 Definition doneTesting (st : State) (f : nat -> RandomSeed -> QProp) : Result :=
  if expectedFailure st then
     Success (numSuccessTests st + 1) (numDiscardedTests st) (summary st)
-            ("+++ Passed " ++ (show (numSuccessTests st)) ++ " tests")
+            ("+++ Passed " ++ (show (numSuccessTests st)) ++ " tests (" ++ (show (numDiscardedTests st)) ++ " discards)")
   else
     NoExpectedFailure (numSuccessTests st) (summary st)
                       ("*** Failed! Passed " ++ (show (numSuccessTests st))
@@ -287,7 +287,8 @@ Fixpoint runATest (st : State) (f : nat -> RandomSeed -> QProp) (maxSteps : nat)
                                  else "+++ Failed (as expected) ")%string in
             let (numShrinks, res') := localMin st (MkRose res ts) in
             let suf := ("after " ++ (show (S nst)) ++ " tests and "
-                                 ++ (show numShrinks) ++ " shrinks")%string in
+                                 ++ (show numShrinks) ++ " shrinks. ("
+                                 ++ (show ndt) ++ " discards)")%string in
             (* TODO: Output *)
             if (negb (expect res)) then
               Success (nst + 1) ndt (summary st) (tag_text ++ pre ++ suf)
