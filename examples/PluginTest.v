f4a7ea9f17ae4728e0380d2c9a3f828044f72e5b
Leonidas Lampropoulos <lemonidas_13@hotmail.com>
2018-05-29 10:21:25 -0400

    Clean up example

--- examples/PluginTest.v
+++ examples/PluginTest.v
@@ -1,7 +1,5 @@
 From QuickChick Require Import QuickChick.
 
-QuickChickDebug Debug On.
-
 Inductive Tree :=
 | Leaf : Tree
 | Node : nat -> Tree -> Tree -> Tree.
@@ -18,21 +16,11 @@ Inductive checker_test : nat -> Tree -> Prop :=
 | CheckerLeaf : forall n, checker_test n Leaf
 | CheckerNode : forall n t, checker_test O Leaf -> checker_test n t.
 
-Instance dec_opt l t : DecOpt (checker_test l t).
-Admitted.
+Derive DecOpt for (checker_test n t).
 
 Derive ArbitrarySizedSuchThat for (fun p => let (m,tr) := p in checker_test m tr).
 
-Set Printing All.
-Derive DecOpt for (checker_test n t).
-Print DecOptchecker_test.
-
-(* Non recursive test *)
 Derive ArbitrarySizedSuchThat for (fun tr => recursion_test m tr).
-
-
-                                             (* m > n -> goodTree m l -> goodTree m r -> goodTree n (Node m l r). *)
-
 
 
 Instance test_coercion A B (P : A -> B -> Prop) `{Gen B}
