dc65fb8b3647f236e1021ea59eaf88cd66bfecee
Yishuai Li <yishuai@cis.upenn.edu>
2018-08-15 20:11:06 -0400

    Extract Z to big integer

--- test/mutation/mutation.sh
+++ test/mutation/mutation.sh
@@ -4,7 +4,7 @@ MUTATION=mutation
 
 # Extract and compile the example
 coqc -Q ../../src QuickChick ${MUTATION}.v
-ocamlbuild ${MUTATION}.native
+ocamlbuild -pkg zarith ${MUTATION}.native
 
 # Look for mutants and test them
 PATH=../../scripts:$PATH quickchick ./${MUTATION}.native 4
