c3365d3e0fd344b2d626ad059e99e5606bf8f5e9
Leonidas Lampropoulos <lemonidas_13@hotmail.com>
2018-10-14 23:24:30 -0400

    [fuzz] Some longer runs to see if something changes.

---
+++ examples/fuzz-ifc/output_fuzz_seeded_30m/ParseResults.hs
@@ -0,0 +1,91 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+import System.Directory
+import System.FilePath
+import System.IO
+
+import Control.Monad
+import Control.Arrow (second)
+
+import Text.ParserCombinators.Parsec
+
+import Data.List
+import Data.Either
+import Data.Char
+
+import Debug.Trace
+  
+{-- File structure
+
+ QuickChecking ...
+ Extracted ML file ...
+ Compile command ...
+ success (maybe)
+ discard (maybe)
+ fail (maybe)
+ +++ Passed
+ time
+ inputs/outputs
+
+-}
+
+data Mode = Naive | Medium | Smart | Seeded
+  deriving (Eq, Ord, Show)
+
+smode "arbitrary"  = Naive
+smode "naive" = Medium
+smode "smart"  = Smart
+smode "seeded" = Seeded
+
+data TestData = MkData { mode      :: !Mode
+                       , mutant    :: !Int
+                       , execs     :: !Int
+                       , unique    :: !Int
+                       , crashes   :: !Int
+                       , time      :: !Double
+                       } deriving (Eq, Ord, Show)
+
+fileNameP :: GenParser Char st (Mode, Int)
+fileNameP = do
+  string "fuzz_"
+  s <- string "seeded_SSNI_" <|> string "SSNI_"
+  m0 <- smode <$> (try (string "arbitrary") <|> string "arbmedium" <|> try (string "smart"))
+  let m = if head s == 's' then Seeded else m0
+  char '_'
+  xs <- many1 digit
+  let x = read xs :: Int
+  return (m, x)
+
+right (Right x) = x
+
+getIntegerInLine :: String -> String
+getIntegerInLine line =
+  takeWhile isDigit $ dropWhile (not . isDigit) line
+
+parseOutput :: String -> [String] -> TestData
+parseOutput fn allLines =
+  let lines = map (read . getIntegerInLine) allLines
+      execsDone = lines !! 4
+      unique = lines !! 17
+      total = lines !! 28
+      (m, id) = right $ parse fileNameP fn fn
+  in MkData m id execsDone unique total 1800
+
+main = do
+  -- Get all files in the current dir
+  allFiles <- getDirectoryContents "."
+  -- Filter the ones that are output files (.out)
+  let outputs = filter ((== ".out") . takeExtension) allFiles
+  -- Iterate through each file
+  parsedData  <- forM outputs $ \f -> do
+    handle    <- openFile f ReadMode
+    contents  <- hGetContents handle
+--    putStrLn $ show (f, lines contents !! 1)
+    let assocDirName  = getIntegerInLine (lines contents !! 1)
+        assocDirStats = "./" ++ assocDirName
+    handle'   <- openFile assocDirStats ReadMode
+    contents' <- hGetContents handle'
+    return $ parseOutput f $ lines contents'
+  let sorted = sort parsedData
+  let (naive,(medium,smart)) = second (splitAt 20) (splitAt 20 sorted)
+  putStrLn $ show (naive, medium, smart)
