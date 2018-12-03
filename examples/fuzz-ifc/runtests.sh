dd9853fe9d442c7c6ba88905c690a8ac4fb24103
Yishuai Li <yishuai@cis.upenn.edu>
2018-12-03 15:10:49 -0500

    fix scripts

--- examples/fuzz-ifc/runtests.sh
+++ examples/fuzz-ifc/runtests.sh
@@ -1,13 +1,12 @@
-#!/bin/bash 
+#!/bin/bash
 
 for i in `seq 0 19`; do
     for prop in SSNI; do
         for mode in arbitrary; do
-            echo "Handling : ${prop} ${mode} exp_result_fuzz ${i}" 
-            cp Driver.v Tmp.v 
-            echo "FuzzChick (testMutantX prop_${prop}_${mode} exp_result_fuzz ${i})." >> Driver.v
-            coqc Driver.v -Q . QuickChick.ifcbasic &> output/fuzz_seeded_${prop}_${mode}_${i}.out
-            cp Tmp.v Driver.v
+            echo "Handling : ${prop} ${mode} exp_result_fuzz ${i}"
+            cp Driver.v Tmp.v
+	    echo -e "\nFuzzChick (testMutantX prop_${prop}_${mode} exp_result_fuzz ${i})." >> Tmp.v
+            coqc Tmp.v -Q . QuickChick.ifcbasic &> output/fuzz_seeded_${prop}_${mode}_${i}.out
         done
     done
 done
@@ -15,8 +14,8 @@ done
 #for i in `seq 0 19`; do
 #    for prop in SSNI; do
 #        for mode in arbitrary arbmedium smart; do
-#            echo "Handling : ${prop} ${mode} exp_result_random ${i}" 
-#            cp Driver.v Tmp.v 
+#            echo "Handling : ${prop} ${mode} exp_result_random ${i}"
+#            cp Driver.v Tmp.v
 #            echo "QuickChick (testMutantX prop_${prop}_${mode} exp_result_random ${i})." >> Driver.v
 #            coqc Driver.v -Q . QuickChick.ifcbasic &> "output/rand_${prop}_${mode}_${i}.out"
 #            cp Tmp.v Driver.v
--- examples/fuzz-ifc/singletest.sh
+++ examples/fuzz-ifc/singletest.sh
@@ -2,5 +2,5 @@
 
 echo "Handling : $1 $2 exp_result_fuzz $3" 
 cp Driver.v Tmp.v 
-echo "QuickChick (testMutantX prop_$1_$2 exp_result_random $3)." >> Tmp.v
+echo -e "\nQuickChick (testMutantX prop_$1_$2 exp_result_random $3)." >> Tmp.v
 coqc Tmp.v -Q . QuickChick.ifcbasic &> output/rand_$1_$2_$3.out
