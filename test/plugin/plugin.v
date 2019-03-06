f309f76c785eb7c392610d52548a736109856ced
Yishuai Li <yishuai@cis.upenn.edu>
2019-03-05 22:24:49 -0500

    modify plugin test suite

--- test/plugin/plugin.v
+++ test/plugin/plugin.v
@@ -1,8 +1,31 @@
 From QuickChick Require Import QuickChick.
+Set Warnings "-extraction-opaque-accessed,-extraction".
 
-Inductive foo (A : Type) :=
-| bar : A -> foo A -> foo A
-| baz : foo A
+(* TODO: better naming *)
+
+Inductive foo {A : Type} :=
+| bar : A -> foo -> foo
+| baz : foo
 .
 
-Derive Show for foo.
+Derive (Arbitrary, Show) for foo.
+Sample (arbitrary : G foo).
+
+Section Sanity.
+
+  Inductive qux : Type :=
+  | Qux: forall {A: Type}, A -> qux.
+
+  Definition quux: qux -> bool :=
+    fun a => match a with | Qux a => true end.
+
+End Sanity.
+
+Section Failures.
+
+  Set Asymmetric Patterns.
+
+  Fail Definition quux': qux -> bool :=
+    fun a => match a with | Qux a => true end.
+
+End Failures.
