af73d20542f28272bfff01bdd157066cbd291c84
Yishuai Li <yishuai@upenn.edu>
2020-04-14 22:34:11 -0400

    examples: replace cmxs with QCInclude

--- examples/c-mutation/extract.ml
+++ examples/c-mutation/extract.ml
@@ -5,5 +5,5 @@ let plus x y =
   output_string oc (string_of_int x ^ " " ^ string_of_int y);
   close_out oc;
   let str = input_line ic in
-  close_process (ic, oc);
+  ignore (close_process (ic, oc));
   int_of_string str
