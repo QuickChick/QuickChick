3d91545792466d3bc19fd4d6c537f57024f5a330
Leonidas Lampropoulos <lemonidas_13@hotmail.com>
2021-06-10 14:23:49 -0400

    Enums - on to testing

--- src/Enumerators.v
+++ src/Enumerators.v
@@ -17,6 +17,45 @@ Open Scope monad_scope.
 From QuickChick Require Import
      Sets Tactics Producer LazyList RandomQC.
 
+Inductive EnumType (A:Type) : Type :=
+  MkEnum : (nat -> LazyList A) -> EnumType A.
+Arguments MkEnum {A}.
+  
+Definition E := EnumType.
+
+(** * Primitive generator combinators *)
+  
+Definition run {A : Type} (g : E A) :=
+  match g with MkEnum f => f end.
+  
+Definition returnEnum {A : Type} (x : A) : E A :=
+  MkEnum (fun _ => retLazyList x).
+
+Definition bindEnum {A B : Type} (g : E A) (k : A -> E B) : E B :=
+  MkEnum (fun n =>
+            x <- run g n;;
+            run (k x) n).
+
+Instance MonadEnum : Monad E :=
+  { ret := @returnEnum
+  ; bind := @bindEnum }.
+
+Definition sizedEnum {A : Type} (f : nat -> E A) : E A :=
+  MkEnum (fun n => run (f n) n).
+
+Definition resizeEnum {A : Type} (n : nat) (g : E A) : E A :=
+    match g with
+    | MkEnum m => MkEnum (fun _ => m n)
+    end.
+
+Definition semEnumSize {A : Type} (g : E A) (s : nat) : set A := fun x => In_ll x (run g s).
+
+Definition chooseEnum {A : Type} `{ChoosableFromInterval A} (range : A * A) : E A :=
+    MkEnum (fun _ => enumR range). 
+
+Definition sampleEnum (A : Type) (g : E A) : list A :=
+  LazyList_to_list (run g 5).
+
 Program Instance ProducerEnum : Producer E :=
   {
   super := MonadEnum;
@@ -36,7 +75,7 @@ Program Instance ProducerEnum : Producer E :=
         (k : forall (a : A),
             (fun (A : Type) (g : E A) =>
                \bigcup_(size in [set: nat]) semEnumSize g size) A g a -> E B) =>
-      MkGen (fun n => _)
+      MkEnum (fun n => _)
   }.
 Next Obligation.
   remember (run g n) as l.
