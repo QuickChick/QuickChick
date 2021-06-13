57f38ebe31d08e5482e180a703019576bcfb3375
Leonidas Lampropoulos <lemonidas_13@hotmail.com>
2021-06-13 15:11:17 -0400

    Dependent classes (modulo 1 proof)

--- src/DependentClasses.v
+++ src/DependentClasses.v
@@ -22,16 +22,17 @@ Class SizedProofEqs {A : Type} (P : A -> Prop) :=
     spec : \bigcup_(n : nat) iter n <--> P}.
 (* end sizeEqs *)
 
-Class SizedSuchThatCorrect {A : Type} (P : A -> Prop) `{SizedProofEqs A P} (g : nat -> G (option A)) :=
+Class SizedSuchThatCorrect {A : Type} (P : A -> Prop) `{SizedProofEqs A P} {G} `{Producer G} (g : nat -> G (option A)) :=
   { sizedSTCorrect : forall s, isSome :&: semProd (g s) <--> Some @: (iter s) }.
 
-Class SuchThatCorrect {A : Type} (P : A -> Prop) (g : G (option A)) :=
+Class SuchThatCorrect {A : Type} (P : A -> Prop) {G} `{Producer G} (g : G (option A)) :=
   { STCorrect : isSome :&: semProd g <-->  Some @: [set x : A | P x ] }.
 
 (** * Dependent sized generators *)
 
 (* begin genSTSized_class *)
-Class GenSizedSuchThat (A : Type) (P : A -> Prop) := { arbitrarySizeST : nat -> G (option A) }.
+Class GenSizedSuchThat (A : Type) (P : A -> Prop) :=
+  { arbitrarySizeST : nat -> G (option A) }.
 (* end genSTSized_class *)
 
 (** * Monotonicity of denendent sized generators *)
@@ -56,7 +57,8 @@ Class GenSizedSuchThatSizeMonotonicOpt (A : Type)
   
 Class GenSizedSuchThatCorrect (A : Type) (P : A -> Prop)
       `{GenSizedSuchThat A P}
-      `{SizedSuchThatCorrect A P arbitrarySizeST}.
+      `{SizedProofEqs _ P}
+      `{@SizedSuchThatCorrect A P _ G ProducerGen arbitrarySizeST}.
 
 (** * Dependent generators *)
 
@@ -78,12 +80,12 @@ Class GenSuchThatMonotonicOpt (A : Type) (P : A -> Prop) `{GenSuchThat A P}
 
 Class GenSuchThatCorrect {A : Type} (P : A -> Prop) 
       `{GenSuchThat A P}
-      `{SuchThatCorrect A P arbitraryST}.
+      `{@SuchThatCorrect A P G ProducerGen arbitraryST}.
 
 Class GenSuchThatMonotonicCorrect (A : Type) (P : A -> Prop)
       `{GenSuchThat A P}
       `{@SizeMonotonicOpt A G ProducerGen arbitraryST}
-      `{SuchThatCorrect A P arbitraryST}.
+      `{@SuchThatCorrect A P G ProducerGen arbitraryST}.
 
 (** Coercions *)
    
@@ -155,6 +157,8 @@ Proof.
   red in PMon.
   apply semSizedSize in H3.
   apply semSizedSize.
+
+  unfold SizedMonotonicOpt in PSMon.
   destruct (PSMon s1 s1 s2 H2 a). apply H3.
   apply (PMon s2 s1 s2); auto. do 3 red.
   eauto.
@@ -224,6 +228,212 @@ Proof.
   constructor; eauto.
 Qed.
 
+(* Dependent Sized Enumerators *)
+(** * Dependent sized generators *)
+
+(* begin genSTSized_class *)
+Class EnumSizedSuchThat (A : Type) (P : A -> Prop) :=
+  { enumSizeST : nat -> E (option A) }.
+(* end genSTSized_class *)
+
+(** * Monotonicity of denendent sized generators *)
+
+Class EnumSizedSuchThatMonotonic (A : Type)
+      `{EnumSizedSuchThat A} `{forall s, SizeMonotonic (enumSizeST s)}.
+
+
+Class EnumSizedSuchThatMonotonicOpt (A : Type)
+      `{EnumSizedSuchThat A} `{forall s, SizeMonotonicOpt (enumSizeST s)}.
+
+Class EnumSizedSuchThatSizeMonotonic (A : Type)
+      `{EnumSizedSuchThat A}
+      `{@SizedMonotonic _ E ProducerEnum enumSizeST}.
+
+Class EnumSizedSuchThatSizeMonotonicOpt (A : Type)
+      `{EnumSizedSuchThat A}
+      `{@SizedMonotonicOpt A E ProducerEnum enumSizeST}.
+
+
+(** * Correctness of denendent sized generators *)
+  
+Class EnumSizedSuchThatCorrect (A : Type) (P : A -> Prop)
+      `{EnumSizedSuchThat A P}
+      `{SizedProofEqs _ P}
+      `{@SizedSuchThatCorrect A P _ E ProducerEnum enumSizeST}.
+
+(** * Dependent generators *)
+
+(* begin genST_class *)
+Class EnumSuchThat (A : Type) (P : A -> Prop) := { enumSuchThat : E (option A) }.
+(* end genST_class *)
+
+Notation "'enumST' x" := (@enumSuchThat _ x _) (at level 70).
+
+(** * Monotonicity of denendent generators *)
+
+Class EnumSuchThatMonotonic (A : Type) (P : A -> Prop) `{EnumSuchThat A P}
+      `{@SizeMonotonic (option A) E ProducerEnum enumSuchThat}.
+
+Class EnumSuchThatMonotonicOpt (A : Type) (P : A -> Prop) `{EnumSuchThat A P}
+      `{@SizeMonotonicOpt A E ProducerEnum enumSuchThat}.
+
+(** * Correctness of dependent generators *)  
+
+Class EnumSuchThatCorrect {A : Type} (P : A -> Prop) 
+      `{EnumSuchThat A P}
+      `{@SuchThatCorrect A P E ProducerEnum enumSuchThat}.
+
+Class EnumSuchThatMonotonicCorrect (A : Type) (P : A -> Prop)
+      `{EnumSuchThat A P}
+      `{@SizeMonotonicOpt A E ProducerEnum enumSuchThat}
+      `{@SuchThatCorrect A P E ProducerEnum enumSuchThat}.
+
+(** Coercions *)
+   
+Instance EnumSizedSuchThatMonotonicOptOfSizeMonotonic
+         (A : Type) (P : A -> Prop) (Hgen : EnumSizedSuchThat A P)
+         (Hmon : forall s : nat, SizeMonotonicOpt (enumSizeST s))
+: @EnumSizedSuchThatMonotonicOpt A P Hgen Hmon := {}.
+
+Instance EnumSizedSuchThatSizeMonotonicOptOfSizedMonotonic
+         (A : Type) (P : A -> Prop) (Hgen : EnumSizedSuchThat A P)
+         (Hmon : SizedMonotonicOpt enumSizeST)
+: @EnumSizedSuchThatSizeMonotonicOpt A P Hgen Hmon := {}.
+
+Instance EnumSizedSuchThatCorrectOptOfSizedSuchThatCorrect
+         (A : Type) (P : A -> Prop) (H : EnumSizedSuchThat A P)
+         (Heqs : SizedProofEqs P)
+         (Hcorr : SizedSuchThatCorrect P enumSizeST)
+: @EnumSizedSuchThatCorrect A P H Heqs Hcorr := {}.
+
+Instance EnumSuchThatMonotonicOptOfSizeMonotonic
+         (A : Type) (P : A -> Prop) (Hgen : EnumSuchThat A P)
+         (Hmon : SizeMonotonicOpt enumSuchThat)
+: @EnumSuchThatMonotonicOpt A _ Hgen Hmon := {}.
+
+Instance EnumSuchThatCorrectOptOfSuchThatCorrect
+         (A : Type) (P : A -> Prop) (H : EnumSuchThat A P)
+         (Hcorr : SuchThatCorrect P (enumST P))
+: @EnumSuchThatCorrect A P H Hcorr := {}.
+
+Instance SizeMonotonicOptofSizeMonotonicEnum {A} (g : E (option A))
+         {H : SizeMonotonic g} : SizeMonotonicOpt g.
+Proof.
+  intros s1 s2 Hs a.
+  eapply monotonic; eauto.
+Qed.
+
+(** * Coercions from sized to unsized generators *)
+
+(* Enumerators *)
+
+(* begin EnumSuchThatOfBounded *)
+Instance EnumSuchThatOfBounded (A : Type) (P : A -> Prop) (H : EnumSizedSuchThat A P)
+: EnumSuchThat A P := { enumSuchThat := sized enumSizeST }.
+(* end EnumSuchThatOfBounded *)
+
+(* Monotonicity *)
+
+Instance EnumSuchThatMonotonicOfSized (A : Type) (P : A -> Prop)
+         {H : EnumSizedSuchThat A P}
+         `{@EnumSizedSuchThatMonotonic A P H PMon}
+         `{@EnumSizedSuchThatSizeMonotonic A P H PSMon}
+  : @EnumSuchThatMonotonic A P (EnumSuchThatOfBounded _ _ H)
+    (@sizedSizeMonotonic E ProducerEnum _ _
+                         _
+                         PMon PSMon) := {}.
+
+Set Printing All.
+Instance SizeMonotonicOptOfBoundedEnum' (A : Type) (P : A -> Prop)
+         {H : EnumSizedSuchThat A P}
+         `{@EnumSizedSuchThatMonotonicOpt A P H PMon}
+         `{@EnumSizedSuchThatSizeMonotonicOpt A P H PSMon}
+: SizeMonotonicOpt (enumST P).
+Proof.
+  unfold enumSuchThat, EnumSuchThatOfBounded.
+  red. red in PMon.
+  do 2 red. intros.
+  unfold semProdSizeOpt in *.
+  red in PMon.
+  apply semSizedSize in H3.
+  apply semSizedSize.
+  unfold SizedMonotonicOpt in PSMon.
+  unfold semProdSizeOpt in *.
+(*
+  destruct (PSMon s1 s1 s2 H2 a). apply H3.
+  apply (PMon s2 s1 s2); auto. do 3 red.
+  eauto.
+  rewrite /semEnumSize => //=.
+  exists x.
+  eauto.
+Qed.
+ *)
+Admitted.
+
+(* begin SizeMonotonicOptOfBounded *)
+Instance SizeMonotonicOptOfBoundedEnum (A : Type) (P : A -> Prop)
+         (H1 : EnumSizedSuchThat A P)
+         (H2 : SizedProofEqs P) (* XXX change name *)
+         (H3 : forall s : nat, SizeMonotonicOpt (enumSizeST s))
+         (H4 : SizedMonotonicOpt enumSizeST) (* XXX change name *)
+: SizeMonotonicOpt (enumST P).
+(* end SizeMonotonicOptOfBounded *)
+Proof.
+  eapply SizeMonotonicOptOfBoundedEnum'.
+  constructor; eauto.
+  constructor; eauto.
+Qed.
+
+Instance EnumSuchThatMonotonicOptOfSized' (A : Type) (P : A -> Prop)
+         {H : EnumSizedSuchThat A P}
+         `{@EnumSizedSuchThatMonotonicOpt A P H PMon}
+         `{@EnumSizedSuchThatSizeMonotonicOpt A P H PSMon}
+: EnumSuchThatMonotonicOpt A P := {}.
+
+(* Correctness *)
+Instance SuchThatCorrectOfBoundedEnum' (A : Type) (P : A -> Prop)
+         {H : EnumSizedSuchThat A P}
+         `{@EnumSizedSuchThatMonotonicOpt A P H PMon}
+         `{@EnumSizedSuchThatSizeMonotonicOpt A P H PSMon}
+         `{@EnumSizedSuchThatCorrect A P H PSized PCorr}
+: SuchThatCorrect P enumSuchThat.
+Proof.
+  constructor; unfold enumSuchThat, EnumSuchThatOfBounded.
+  rewrite semSized_opt; eauto.
+  split.
+  - intros [H3 H4]. destruct a; try discriminate.
+    eexists. split; [| reflexivity ].
+    eapply spec.
+    destruct H4 as [n [_ Hsem]]. 
+    exists n. split. now constructor.
+    assert (Ha : (isSome :&: semProd (enumSizeST n)) (Some a)).
+    { split; eauto. }
+    eapply PCorr in Ha. destruct Ha as [a' [Hit Heq]]. inv Heq. eassumption.
+  - intros [y [HP Heq]]. inv Heq.
+    eapply spec in HP. destruct HP as [n [_ Hit]].
+    split; eauto. exists n. split; [ now constructor |].
+    eapply PCorr. eexists; split; eauto.
+Qed.
+
+(* begin SuchThatCorrectOfBounded *)
+Instance SuchThatCorrectOfBoundedEnum (A : Type) (P : A -> Prop)
+         (H1 : EnumSizedSuchThat A P)
+         (H2 : SizedProofEqs P) (* XXX change name *)
+         (H3 : forall s : nat, SizeMonotonicOpt (enumSizeST s))
+         (H4 : SizedMonotonicOpt enumSizeST) (* XXX change name *)
+         (H5 : SizedSuchThatCorrect P enumSizeST)
+: SuchThatCorrect P enumSuchThat.
+(* end SuchThatCorrectOfBounded *)
+Proof.
+  eapply SuchThatCorrectOfBoundedEnum'; eauto.
+  constructor; eauto.
+  constructor; eauto.
+  constructor; eauto.
+Qed.
+
+
+
+
 (* TODO: Move to another file *)
 (*
 (** Leo's example from DependentTest.v *)
@@ -248,24 +458,24 @@ Inductive goodFooNarrow : nat -> Foo -> Prop :=
 Print genGoodNarrow. 
 
 (** For dependent gens we show generate this instance *)
-Instance genGoodNarrow (n : nat) : ArbitrarySizedSuchThat Foo (goodFooNarrow n) :=
+Instance genGoodNarrow (n : nat) : EnumSizedSuchThat Foo (goodFooNarrow n) :=
   {
-    arbitrarySizeST := genGoodNarrow' n;
+    enumSizeST := genGoodNarrow' n;
     shrinkSizeST x := []
   }.
 
 (* For proofs we should generate this instances *)
 
 Instance genGoodNarrowMon (n : nat) (s : nat) :
-  SizeMonotonic (@arbitrarySizeST Foo (goodFooNarrow n) _ s).
+  SizeMonotonic (@enumSizeST Foo (goodFooNarrow n) _ s).
 Abort.
 
 Instance genGoodNarrowSMon (n : nat) :
-  @ArbitrarySTSizedSizeMotonic Foo (@goodFooNarrow n) _.
+  @EnumSTSizedSizeMotonic Foo (@goodFooNarrow n) _.
 Abort.
 
 Instance genGoodNarrowCorr (n : nat) :
-  GenSizeSuchThatCorrect (goodFooNarrow n) (@arbitrarySizeST Foo (goodFooNarrow n) _).
+  EnumSizeSuchThatCorrect (goodFooNarrow n) (@enumSizeST Foo (goodFooNarrow n) _).
 Abort.
 *)
 
