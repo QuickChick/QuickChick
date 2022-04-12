11a3b44b700afe1bbf1c3dd17786195f2842450f
Li-yao Xia <lysxia@gmail.com>
2022-04-11 22:47:01 -0400

    ci: simplify

--- test/mutation/run.sh
+++ test/mutation/run.sh
@@ -5,7 +5,7 @@ MUTATION=mutation
 # Extract and compile the example
 coqc ${MUTATION}.v
 EXTRACTED=mutation.ml
-ocamlbuild -pkg zarith,pure-splitmix,coq-simple-io.extraction ${MUTATION}.native
+ocamlfind opt -package zarith,pure-splitmix,coq-simple-io.extraction -linkpkg -o ${MUTATION}.native ${MUTATION}.mli ${MUTATION}.ml
 
 # Look for mutants and test them
 PATH=../../scripts:$PATH quickchick ./${MUTATION}.native 4
