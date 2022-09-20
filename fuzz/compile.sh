6e47a3054c601856eb8479d6abd3f7e712d84507
Yishuai Li <liyishuai.lys@alibaba-inc.com>
2022-09-20 16:25:15 +0800

    Fix compilation on macOS

--- fuzz/compile.sh
+++ fuzz/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ocamlopt -afl-instrument C.ml
-ocamlopt C.cmx -o foo Fuzz.ml SHM.c
-ocamlopt -o main Main.ml SHM.c
+ocamlopt -ccopt -Wno-error=implicit-function-declaration C.cmx -o foo Fuzz.ml SHM.c
+ocamlopt -ccopt -Wno-error=implicit-function-declaration -o main Main.ml SHM.c
